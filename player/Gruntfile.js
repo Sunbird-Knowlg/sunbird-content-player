eb7c5d311e3b6ea2ae1512ec75208016a8c28817
Mahesh <mahesh@canopusconsulting.com>
2015-10-07 23:58:32 +0530

    created grunt task to build all(lollipop, x86, armv7) release, debug apks with one command.
    grunt build-all-apks

--- player/Gruntfile.js
+++ player/Gruntfile.js
@@ -233,5 +233,6 @@ module.exports = function(grunt) {
     grunt.registerTask('install-apk-quick', ['uglify:js', 'clean:before', 'copy', 'rename', 'clean:after', 'clean:samples', 'cordovacli:run_android']);
     grunt.registerTask('rm_custom_plugins', ['cordovacli:rm_custom_plugins']);
     grunt.registerTask('add_custom_plugins', ['cordovacli:add_custom_plugins']);
+    grunt.registerTask('build-all-apks', ['uglify:js', 'clean:before', 'copy', 'rename', 'clean:after', 'clean:samples', 'cordovacli:add_plugins', 'cordovacli:build_android', 'cordovacli:build_android_release', 'cordovacli:rm_xwalk', 'cordovacli:build_android', 'cordovacli:build_android_release']);
     grunt.registerTask('update_custom_plugins', ['cordovacli:rm_custom_plugins', 'cordovacli:add_custom_plugins']);
 };
