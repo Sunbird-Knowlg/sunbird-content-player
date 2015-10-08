080357306ab270455bfa521419c0481f01ab9622
Mahesh <mahesh@canopusconsulting.com>
2015-10-08 22:43:15 +0530

    sprite sheet data getting from ecml.
    Below is the sample:
            <media id="grant_sprite" src="spritesheet_grant.png" type="image" />
    
            <sprite asset="grant_sprite" start="jump_run" x="80" y="15" id="sprite_jump" framerate="30">
                <frames regX="82" regY="0" width="165" height="292" count="64" />
                <animations>
                    <stand>[60]</stand>
                    <run>[0, 25, "run", 1.5]</run>
                    <jump>[26, 63, "jump"]</jump>
                    <jump_run>[26, 63, "jump"]</jump_run>
                </animations>
            </sprite>

--- player/public/js/app/plugin/SpritePlugin.js
+++ player/public/js/app/plugin/SpritePlugin.js
@@ -1,19 +1,35 @@
 var SpritePlugin = Plugin.extend({
-	_isContainer: false,
-	_render: true,
-	initPlugin: function(data) {
-		var dims = this.relativeDims();
-		var spritesheet = this._theme.getAsset(data.asset);
-		var grant = new createjs.Sprite(spritesheet, data.start);
-		grant.x = dims.x;
-		grant.y = dims.y;
-		this._self = grant;
-		grant.addEventListener('change', function() {
-			Renderer.update = true;
-		});
-	},
-	playAnimation: function(animation) {
-		this._self.gotoAndPlay(animation);
-	}
+    _isContainer: false,
+    _render: true,
+    initPlugin: function(data) {
+        var dims = this.relativeDims();
+        var spriteImage = this._theme.getAsset(data.asset);
+        var animations = {};
+        if(data.animations) {
+            for(k in data.animations) {
+                animations[k] = JSON.parse(data.animations[k]);
+            }
+        }
+        var spriteJSON = {
+            "framerate": data.framerate,
+            "images": [spriteImage],
+            "frames": data.frames,
+            "animations": animations
+        };
+        var spritesheet = new createjs.SpriteSheet(spriteJSON);
+        var grant = new createjs.Sprite(spritesheet);
+        if(data.start) {
+            grant.gotoAndPlay(data.start);
+        }
+        grant.x = dims.x;
+        grant.y = dims.y;
+        this._self = grant;
+        grant.addEventListener('change', function() {
+            Renderer.update = true;
+        });
+    },
+    playAnimation: function(animation) {
+        this._self.gotoAndPlay(animation);
+    }
 });
 PluginManager.registerPlugin('sprite', SpritePlugin);
