2893a02f69c40a1363b62e4eb923b7b8a229f347
jitendrasinghsankhwar <jitendras@ilimi.in>
2016-02-08 15:21:03 +0530

    Update TelemetryEvent.js

--- telemetry/TelemetryEvent.js
+++ telemetry/TelemetryEvent.js
@@ -47,13 +47,13 @@ TelemetryEvent = Class.extend({
     },
     start: function() {
         this._isStarted = true;
-        this.startTime = Date.now();
+        this.startTime = new Date().getTime();
         return this;
     },
     end: function() {
         if (this._isStarted) {
-            this.event.edata.eks.length = Math.round((Date.now() - this.startTime ) / 1000);
-            this.event.ets = Date.now();
+            this.event.edata.eks.length = Math.round((new Date().getTime() - this.startTime ) / 1000);
+            this.event.ets = new Date().getTime();
             return this;
         } else {
             throw "can't end event without starting.";
