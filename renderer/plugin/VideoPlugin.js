b2f94d13a78349511d0cd9551d86eb8cd15c39fb
Vinu Kumar <vinu.kumar@tarento.com>
2016-03-10 20:15:18 +0530

    #229 Handled multiple video support in the single stage

--- renderer/plugin/VideoPlugin.js
+++ renderer/plugin/VideoPlugin.js
@@ -14,24 +14,34 @@ var VideoPlugin = Plugin.extend({
     },
     loadVideo: function (){
         var lItem =  this._createDOMElementVideo();
-        this._self = new createjs.Bitmap(lItem);//new createjs.DOMElement(document.createElement('p'));
+        this._self = new createjs.Bitmap(lItem);
 
-        if(_videoEle.readyState >= 2){   
-            this.replay();
+        //If autoplay set to true, then play video
+        if(this._data.autoplay == true){
+            this.play();
         }
     }, 
     play: function(action){
+        if(action){
+            _videoEle = this.getVideo(action.asset);
+        }
         _videoEle.style.display = "block";
-        this._self.paused = false;
+        //this._self.paused = false;
         _videoEle.play();
         this.sendTelemeteryData(action, "PLAY");
     },
     pause: function(action) {
-        this._self.paused = true;
+        if(action){
+            _videoEle = this.getVideo(action.asset);
+        }
+        //this._self.paused = true;
         _videoEle.pause();
         this.sendTelemeteryData(action, "PAUSE");
     },
     stop: function(action) {
+        if(action){
+            _videoEle = this.getVideo(action.asset);
+        }
         _videoEle.currentTime = 0;
         _videoEle.pause();
         this.sendTelemeteryData(action, "STOP");
@@ -41,6 +51,7 @@ var VideoPlugin = Plugin.extend({
         console.log("video end..");
     },
     replay: function(){
+        _videoEle = this.getVideo(this._data.asset);
         _videoEle.currentTime = 0;
         this.play();
     },
@@ -48,53 +59,64 @@ var VideoPlugin = Plugin.extend({
         if(action)
             EventManager.processAppTelemetry(action, 'LISTEN', this._instance, {subtype : subType});
     },
+    getVideo: function(videoId){
+        return document.getElementById(videoId);
+    },
     _createDOMElementVideo: function () {
 
         var videoAssest = this._theme.getAsset(this._data.asset);
         var dims = this.relativeDims();
-        console.log(dims);
+        
+        console.log(videoAssest);
 
         var jqVideoEle = jQuery(videoAssest).insertBefore("#gameArea");
-        jQuery(jqVideoEle).attr({type: "video/webm", id: "video1"})
+        jQuery(jqVideoEle).attr("type", this._data.type);
+        jQuery(jqVideoEle).attr("id", this._data.asset)
         .prop({autoplay: this._data.autoplay, controls: this._data.controls, width: dims.w, height: dims.h})
-        .css({position: 'absolute', display: "none", left: dims.x + "px", top: dims.y + "px"});
+        .css({position: 'absolute', left: dims.x + "px", top: dims.y + "px"});
 
         //Pushing video element to the stage HTML elements list
         // So when stage is chagned, remove all HTML elements of previous stage
         this._theme.htmlElements.push(jQuery(jqVideoEle).attr('id'));
 
-        _videoEle = document.getElementById('video1');
+        var videoEle = document.getElementById(this._data.asset);
 
-        _videoEle.addEventListener("error", function (evt) {
+        videoEle.addEventListener("error", function (evt) {
         var lErrMesg = "Error loading video element, event.type [" + evt.type + "]  Media Details: [" + evt.target.src + "]";
             console.log(lErrMesg);
 
         });
 
-        _videoEle.addEventListener("abort", function (evt) {
+        videoEle.addEventListener("abort", function (evt) {
             var lErrMesg = "Abort/Error loading video element, event.type [" + evt.type + "] Media Details: [" + evt.target.src + "]";
             console.log(lErrMesg);
         });
 
-        _videoEle.addEventListener("loadeddata", function (evt) {
+        videoEle.addEventListener("loadeddata", function (evt) {
             var lMesg = "Media element can be played, event.type [" + evt.type + "] Media Details: [" + evt.target.src + "]";
-            _instance.play();
+            if(_instance.autoplay == true){
+                _instance.play();
+            }
+           
             console.log(lMesg);
         });
 
-        _videoEle.addEventListener('ended', function (evt){
+        videoEle.addEventListener('ended', function (evt){
             _instance.end();
         });
 
-        /*function draw(v,c,w,h) {
-            if(v.paused || v.ended) return false;
-            c.drawImage(v,0,0,w,h);
-            setTimeout(draw,20,v,c,w,h);
-        }*/
         var div = document.getElementById('gameArea');
-        div.insertBefore(_videoEle, div.childNodes[0]);
+        div.insertBefore(videoEle, div.childNodes[0]);
         
-        return new createjs.Bitmap(_videoEle);
+        if(videoEle.readyState >= 2){ 
+            if(this._data.autoplay == true)  {
+                this.replay();
+            }
+        }
+
+        _videoEle = videoEle;
+       
+        return new createjs.Bitmap(videoEle);
     } // end_function createDOMElementVideo
  });
  PluginManager.registerPlugin('video', VideoPlugin);
