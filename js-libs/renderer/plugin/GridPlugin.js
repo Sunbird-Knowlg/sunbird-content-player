5ef9d774fdf0f220bec75337cda755fc3bb66755
Vinu Kumar <vinu.kumar@tarento.com>
2016-03-18 16:05:23 +0530

    #237 Inside GridCell, added plugins alignment issues fixed. Able to render Text, Image and Shape

--- js-libs/renderer/plugin/GridPlugin.js
+++ js-libs/renderer/plugin/GridPlugin.js
@@ -49,39 +49,25 @@ var GridPlugin = Plugin.extend({
 			//Adding grid rows
 			for(var i=0; i<this._properties.colCount; i++){
 
+				//Creating cell container
+				var cellContObj = {} 
+				cellContObj.w = 100/this._properties.colCount;
+				cellContObj.h = 100/this._properties.rowCount;
+				cellContObj.x = this._properties.colWidth * i;
+				cellContObj.y = this._properties.rowHeight * j;
+
+				var cellCont = PluginManager.invoke("g", cellContObj, this, this._stage, this._theme);
+
+				cellCont._self.x = cellContObj.x;
+				cellCont._self.y = cellContObj.y;
+
 				for(var k=0; k<this._dataItems.length; k++){
 					var dataItem = this._getDataItem(this._dataItems[k], this._dataModel[_cellCount]);
-					//dataItem.type = "rect";
-					//dataItem.$t = "Row-"+j +", Cell-" + i;
-					dataItem.w = 100/this._properties.colCount;
-					dataItem.h = 100/this._properties.rowCount;
-					dataItem.x = this._properties.colWidth * i;
-					dataItem.y = this._properties.rowHeight * j;
-					//dataItem.fill = "grey";
-					//dataItem.stroke="black"
-
-					//console.log(dataItem);
-					var rowContainer = PluginManager.invoke(dataItem.pluginType, dataItem, this, this._stage, this._theme);
+					var pluginItem = PluginManager.invoke(dataItem.pluginType, dataItem, cellCont, cellCont._stage, cellCont._theme);
 					
-					rowContainer.invokeChildren(dataItem, rowContainer, this._stage, this._theme);
+					//pluginItem.invokeChildren(dataItem, pluginItem, this._stage, this._theme);
 
-					var conDims = rowContainer.relativeDims();
-					rowContainer._self.x = dataItem.x;
-					rowContainer._self.y = dataItem.y;
-					/*rowContainer._self.w = dataItem.w;//conDims.w/this._properties.colCount;
-					rowContainer._self.h = dataItem.h;*/
-
-	 			  // //Alternate row colors
-					// var fillColor = "#5DCBFD"
-					// if(i%2 == 0){
-					// 	fillColor = "#D7ECF4"
-					// }
-
-					// //Adding shpae to row to set row bg color, stroke etc..
-					// var rowShape = new createjs.Shape();
-					// var cellDims = rowContainer.relativeDims();
-
-					// rowShape.graphics.beginFill(fillColor).r(dataItem.x, dataItem.y, cellDims.w, cellDims.h);
+					var conDims = pluginItem.relativeDims();
 				}
 				_cellCount++;
 			}
@@ -93,27 +79,28 @@ var GridPlugin = Plugin.extend({
 
 		//Getting json data for the row
 		//var dataObj = this._dataModel[rowIndex];
-		
-		if(dataItem.pluginType == "text"){
-			dataItem.$t = dataObj[dataItem.model];
-		}else if(dataItem.pluginType == "image"){
-			dataItem.asset = dataObj[dataItem.model];
-		}/*else if(dataItem.pluginType == "g"){
-			var childItems = this._getDataItems(dataItem);
+		var dataItemCopy = _.clone(dataItem);
+		if(dataItemCopy.pluginType == "text"){
+			dataItemCopy.$t = dataObj[dataItemCopy.model];
+			dataItemCopy.fontsize = dataItemCopy.fontsize * this._properties.colCount;
+		}else if(dataItemCopy.pluginType == "image"){
+			dataItemCopy.asset = dataObj[dataItemCopy.model];
+		}/*else if(dataItemCopy.pluginType == "g"){
+			var childItems = this._getdataItemCopys(dataItemCopy);
 			for(k in childItems){
 				childItem = childItems[k];
 				if(childItem.model != null){
-					childItem = this._getDataItem(childItem, dataObj);
-					_.find(dataItem, )
+					childItem = this._getdataItemCopy(childItem, dataObj);
+					_.find(dataItemCopy, )
 					console.log("childItem", childItem);
 				}
 			}
-			console.log("g children", dataItem);
+			console.log("g children", dataItemCopy);
 		}*/
 
-		_.omit(dataItem, dataItem.model);
-		//console.log("dataItem", dataItem);
-		return dataItem;
+		_.omit(dataItemCopy, dataItemCopy.model);
+		//console.log("dataItemCopy", dataItemCopy);
+		return dataItemCopy;
 		//return _.findWhere(this._dataItems, {"order": orderIndex+1});
 	},
 	_getDataItems: function(inputData){
