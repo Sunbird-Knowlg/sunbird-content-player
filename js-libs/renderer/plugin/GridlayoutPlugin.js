8372fbcc628a95e10a8b82af5d8d2a9359f9b9cd
Mahesh <mahesh@canopusconsulting.com>
2016-03-22 18:53:35 +0530

    Grid-layout plugin code refactoring.

--- js-libs/renderer/plugin/GridlayoutPlugin.js
+++ js-libs/renderer/plugin/GridlayoutPlugin.js
@@ -1,35 +1,38 @@
-var GridlayoutPlugin = Plugin.extend({
+var GridlayoutPlugin = LayoutPlugin.extend({
 	_type: 'gridlayout',
-    _render: true,
-    initPlugin: function(data) {
-    	this._self = new createjs.Container();
-		var dims = this.relativeDims();
-        this._self.x = dims.x;
-        this._self.y = dims.y;
+    generateLayout: function() {
+    	var tableProps = this.getTableProperties();
+    	var instance = this;
+    	var marginX = 0;
+    	if (_.isFinite(this._data.marginX)) {
+    		marginX = this._data.marginX;
+    	}
+    	var marginY = 0;
+    	if (_.isFinite(this._data.marginY)) {
+    		marginY = this._data.marginY;
+    	}
 
-        var hit = new createjs.Shape();
-        hit.graphics.beginFill("#000").r(0, 0, dims.w, dims.h);
-        this._self.hitArea = hit;
-
-    	var model = data.iterate;
-    	var dataObjs = this._stage.getModelValue(model);
-    	// console.log("dataObjs:", dataObjs);
-    	this.renderTableLayout(dataObjs);
+    	var cw = (100 - ((tableProps.cols-1) * marginX))/tableProps.cols;
+    	var ch = (100 - ((tableProps.rows-1) * marginY))/tableProps.rows;
+    	var index = 0;
+        
+    	for (var r=0; r<tableProps.rows; r++) {
+    		for (var c=0; c<tableProps.cols; c++) {
+    			if (this._cells.length < this._cellsCount) {
+    				var data = {};
+    				data.x = (c * (cw + marginX));
+    				data.y = (r * (ch + marginY));
+    				data.w = cw;
+    				data.h = ch;
+                    this._cells.push(data);
+    			}
+    		}
+    	}
     },
-    getLayout: function() {
-    	var children = {};
-    	var data = this._data;
-        for (k in data) {
-	        if (PluginManager.isPlugin(k)) {
-	        	children[k] = data[k];
-	        }
-        }
-        return children;
-    },
-    renderTableLayout: function(value) {
-    	var cols = undefined;
+    getTableProperties: function() {
+        var cols = undefined;
         var rows = undefined;
-    	var count = value.length;
+        var count = this._cellsCount;
         if(this._data.rows && this._data.cols) {
             cols = this._data.cols; 
             rows = Math.ceil(count/cols);
@@ -42,63 +45,8 @@ var GridlayoutPlugin = Plugin.extend({
                 cols = Math.ceil(count/rows);
             else
                 rows = Math.ceil(count/cols);
-        }    
-    	var instance = this;
-    	var marginX = 0;
-    	if (_.isFinite(this._data.marginX)) {
-    		marginX = this._data.marginX;
-    	}
-    	var marginY = 0;
-    	if (_.isFinite(this._data.marginY)) {
-    		marginY = this._data.marginY;
-    	}
-    	var padX = this._data.padX || 0;
-        var padY = this._data.padY || 0;
-    	var cw = (100 - ((cols-1) * marginX))/cols;
-    	var ch = (100 - ((rows-1) * marginY))/rows;
-    	var index = 0;
-    	for (var r=0; r<rows; r++) {
-    		for (var c=0; c<cols; c++) {
-    			if (c*r < count) {
-    				var data = {};
-    				data.x = (c * (cw + marginX));
-    				data.y = (r * (ch + marginY));
-    				data.w = cw;
-    				data.h = ch;
-    				data.padX = padX;
-                    data.padY = padY;
-                    data.snapX = instance._data.snapX;
-                    data.snapY = instance._data.snapY;
-
-                    data.stroke = instance._data.stroke;
-                    data['stroke-width'] = instance._data['stroke-width'];
-                    data.events = instance._data.events;
-                    data.event = instance._data.event;
-
-                    if (this._parent._shadow) {
-                        data.shadowColor = this._parent._shadow;
-                    }
-                    if (this._parent._highlight) {
-                        data.highlight = this._parent._highlight;
-                    }
-                    if (_.isFinite(this._parent._blur)) {
-                        data.blur = this._parent._blur;
-                    }
-                    if (_.isFinite(this._parent._offsetX)) {
-                        data.offsetX = this._parent._offsetX;
-                    }
-                    if (_.isFinite(this._parent.offsetY)) {
-                        data.offsetY = this._parent.offsetY;
-                    }
-                    if(this._data.opacity)
-                        data.opacity = this._data.opacity;
-                    Object.assign(data, this.getLayout());
-                    instance._stage._templateVars[this._data['var']] = this._data.iterate+"["+index+"]";
-                    index++;
-    				PluginManager.invoke('g', data, instance, instance._stage, instance._theme);
-    			}
-    		}
-    	}
+        }
+        return {"cols": cols, "rows": rows};
     }
 });
 PluginManager.registerPlugin('grid', GridlayoutPlugin);
