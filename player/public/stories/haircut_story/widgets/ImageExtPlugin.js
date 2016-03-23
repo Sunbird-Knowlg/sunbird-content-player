e07d9368baee34b3dc97a3632d730959a1529434
Mahesh <mahesh@canopusconsulting.com>
2016-03-23 15:39:31 +0530

    ImageExtPlugin.js and bug_data.json included.

---
+++ player/public/stories/haircut_story/widgets/ImageExtPlugin.js
@@ -0,0 +1,15 @@
+Plugin.extend({
+    _type: 'image',
+    _isContainer: false,
+    _render: true,
+    initPlugin: function(data) {
+        var instance = this;
+        var s = new createjs.Bitmap(this._theme.getAsset(data.asset));
+        var dims = this.relativeDims();
+        var sb = s.getBounds();
+        s.x = dims.x;
+        s.y = dims.y;
+        this._self = s;
+        if(sb) this.setScale(); 
+    }
+});
