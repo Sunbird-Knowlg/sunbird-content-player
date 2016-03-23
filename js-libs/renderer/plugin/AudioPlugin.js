ede11551d1e3744fc4585d27676408a825e02e89
Jitendra Singh Sankhwar <jitendras@ilimi.in>
2016-03-23 11:42:29 +0530

    audio plugin changes to call audio manager.

--- js-libs/renderer/plugin/AudioPlugin.js
+++ js-libs/renderer/plugin/AudioPlugin.js
@@ -8,44 +8,58 @@ var AudioPlugin = Plugin.extend({
         this._id = data.asset;
     },
     play: function(action) {
-        this.stageId = this._stage._id;
-        EventManager.processAppTelemetry(action, 'LISTEN', this);
-        if(this._state == 'paused') {
-            this._self.paused = false;
-            this._state = 'play';
-        } else if(this._self) {
-            this._state = 'play';
-            this._self.play();
-        } else {
-            this._state = 'play';
-            var instance = this;
-            this._self = createjs.Sound.play(this._id);
-            this._self.on("complete", function() {
-                instance._state = 'stop';
-            });
-        }
+
+        AudioManager.play(action);
+        // this.stageId = this._stage._id;
+        // EventManager.processAppTelemetry(action, 'LISTEN', this, {subtype : "PLAY"});
+        // if(this._state == 'paused') {
+        //     this._self.paused = false;
+        //     this._state = 'play';
+        // } else if(this._self) {
+        //     this._state = 'play';
+        //     this._self.play();
+        // } else {
+        //     this._state = 'play';
+        //     var instance = this;
+        //     this._self = createjs.Sound.play(this._id);
+        //     this._self.on("complete", function() {
+        //         instance._state = 'stop';
+        //     });
+        // }
     },
     togglePlay: function(action) {
-        if(this._state == 'play') {
-            this.pause(action);
-        } else {
-            this.play(action);
-        }
+       AudioManager.togglePlay(action);
+
+        // if(this._state == 'play') {
+        //     this.pause(action);
+        // } else {
+        //     this.play(action);
+        // }
     },
     pause: function(action) {
-        if(this._state == 'play') {
-            this._self.paused = true;
-            this._state = 'paused';
-            this.stageId = this._stage._id;
-            EventManager.processAppTelemetry(action, 'PAUSE_LISTENING', this);
-        }
+        
+        AudioManager.pause(action);
+        // if(this._state == 'play') {
+        //     this._self.paused = true;
+        //     this._state = 'paused';
+        //     this.stageId = this._stage._id;
+        //     // EventManager.processAppTelemetry(action, 'PAUSE_LISTENING', this);
+        //     EventManager.processAppTelemetry(action, 'LISTEN', this, {subtype : "PAUSE"});
+        // }
     },
     stop: function(action) {
-        if(this._state == 'play') {
-            this._self.stop();
-            this.stageId = this._stage._id;
-            EventManager.processAppTelemetry(action, 'STOP_LISTENING', this);
+        
+        if (action.sound === true) {
+            AudioManager.stopAll(action);
+        } else {
+            AudioManager.stop(action);
         }
+        // if(this._state == 'play') {
+        //     this._self.stop();
+        //     this.stageId = this._stage._id;
+        //     // EventManager.processAppTelemetry(action, 'STOP_LISTENING', this);
+        //     EventManager.processAppTelemetry(action, 'LISTEN', this, {subtype : "STOP"});
+        // }
     }
 
 });
