9f6cd022a8a8724633c628c17a654506425b587f
Mahesh <mahesh@canopusconsulting.com>
2016-04-01 16:23:48 +0530

    changed priority of checking asset value from data object #260

--- js-libs/renderer/plugin/ImagePlugin.js
+++ js-libs/renderer/plugin/ImagePlugin.js
@@ -5,20 +5,25 @@ var ImagePlugin = Plugin.extend({
     initPlugin: function(data) {
         var instance = this;
         var asset = '';
-        if (data.model) {
-            asset = this._stage.getModelValue(data.model);   
+        if(data.asset) {
+            asset = data.asset;
+        } else if (data.model) {
+            asset = this._stage.getModelValue(data.model);
         } else if (data.param) {
             asset = this.getParam(data.param);
-        } else {
-            asset = data.asset;
         }
-        var s = new createjs.Bitmap(this._theme.getAsset(asset));
-        var dims = this.relativeDims();
-        var sb = s.getBounds();
-        s.x = dims.x;
-        s.y = dims.y;
-        this._self = s;
-        if(sb) this.setScale(); 
+        if(_.isEmpty(asset)) {
+            this._render = false;
+            console.warn("asset not found", data);
+        } else {
+            var s = new createjs.Bitmap(this._theme.getAsset(asset));
+            var dims = this.relativeDims();
+            var sb = s.getBounds();
+            s.x = dims.x;
+            s.y = dims.y;
+            this._self = s;
+            if(sb) this.setScale();    
+        }        
     }
 });
 PluginManager.registerPlugin('image', ImagePlugin);
