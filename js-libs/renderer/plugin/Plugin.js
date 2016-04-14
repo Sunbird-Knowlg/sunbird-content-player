84e8f7cb5f0550c402878f2fb85999b36dbb5507
Mahesh <mahesh@canopusconsulting.com>
2016-04-15 00:42:00 +0530

    Skipping render if there is no createjs display object.

--- js-libs/renderer/plugin/Plugin.js
+++ js-libs/renderer/plugin/Plugin.js
@@ -114,7 +114,11 @@ var Plugin = Class.extend({
 		this._self.removeChild(child);
 	},
 	render: function() {
-		this._parent.addChild(this._self, this);
+        if (this._self) {
+            this._parent.addChild(this._self, this);
+        } else {
+            console.warn("Skipped rendering object because, no element:", this._id);
+        }
 	},
 	update: function() {
 		this._theme.update();
