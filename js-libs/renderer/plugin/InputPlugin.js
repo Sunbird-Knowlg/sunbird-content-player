fb6269d817038709674794a944ff5cb2f5c83685
feroz <feroz@canopusconsulting.com>
2016-05-23 22:48:14 +0530

    Added the following attributes to input plugin - font, fontsize, weight, color, fill, stroke - their meaning same as that for text plugin.

--- js-libs/renderer/plugin/InputPlugin.js
+++ js-libs/renderer/plugin/InputPlugin.js
@@ -5,6 +5,15 @@ var InputPlugin = Plugin.extend({
     _input: undefined,
 	initPlugin: function(data) {
         this._input = undefined;
+
+        var fontsize = data.fontsize || "1.6em";
+        var fontweight = data.weight || "normal";
+        var color = data.color || "#000000";
+        var background = data.fill || "white";
+        var font = data.font || "Arial";
+        var border = data.stroke || "#000000";
+        data.stroke = ""; // unset so that parent plugin doesn't draw the border
+
 		var dims = this.relativeDims();
 		var input = document.getElementById(data.id);
 		if(input) {
@@ -13,10 +22,18 @@ var InputPlugin = Plugin.extend({
         input = document.createElement('input');
         input.id = data.id;
         input.type = data.type;
+
         input.style.width = dims.w + 'px';
         input.style.height = dims.h + 'px';
         input.style.minWidth = dims.w + 'px';
         input.style.minHeight = dims.h + 'px';
+        input.style.setProperty("font-size", fontsize, "important");
+        input.style.setProperty("font-weight", fontweight, "important");
+        input.style.setProperty("font-family", font, "important");
+        input.style.setProperty("color", color, "important");
+        input.style.setProperty("background-color", background, "important");
+        input.style.setProperty("border-color", border, "important");
+
         input.className = data.class;
         input.style.display = 'none';
         var instance = this;
