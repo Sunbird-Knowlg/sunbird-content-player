509808c239b81589ce39e0a30b20497516346543
Mahesh <mahesh@canopusconsulting.com>
2016-05-26 19:03:56 +0530

    highlight-text without audio.

--- js-libs/renderer/plugin/HighlightTextPlugin.js
+++ js-libs/renderer/plugin/HighlightTextPlugin.js
@@ -3,14 +3,20 @@ var HighlightTextPlugin = HTMLPlugin.extend({
     _wordIds: [],
     _timings: [],
     _isPlaying: false,
+    _isPaused: false,
     _wordClass: "gc-ht-word",
     _listener: undefined,
     _audioInstance: undefined,
+    _position: {
+        previous: 0,
+        current: 0,
+        pause: 0
+    },
+    _time: 0,
     initPlugin: function(data) {
         var dims = this.relativeDims();
         if (!data.id) data.id = this._data.id = _.uniqueId('plugin');
         if (!data.highlight) data.highlight = this._data.highlight = "#DDDDDD";
-        // if (!data["highlight-color"]) data["highlight-color"] = this._data["highlight-color"] = "#000";
         var div = document.createElement('div');
         div.id = data.id;
         div.style.width = dims.w + 'px';
@@ -22,7 +28,6 @@ var HighlightTextPlugin = HTMLPlugin.extend({
             div.style["font-size"] = "1.2em";
         }
 
-        var instance = this;
         var parentDiv = document.getElementById(Renderer.divIds.gameArea);
         parentDiv.insertBefore(div, parentDiv.childNodes[0]);
 
@@ -49,74 +54,71 @@ var HighlightTextPlugin = HTMLPlugin.extend({
         var instance = this;
         var audio = action.audio || this._data.audio;
         if (this._timings.length > 0) {
-            this._isPlaying = true;
-            if (audio) {
-                this._data.audio = audio;
-                if (instance._audioInstance) {
-                    instance.resume(action);
-                } else {
-                    instance._audioInstance = AudioManager.play({asset: audio, stageId: this._stage._id});
-                    var prevPosition = 0;
-                    this._listener = function() {
-                        var position = Number(instance._audioInstance.object.position.toFixed(0));
-                        if (position && instance._isPlaying) {
-                            var matches = _.filter(instance._timings, function(time) {
-                                return (time >= prevPosition && time < position);
-                            });
-                            if (matches.length >0) {
-                                _.each(matches, function(match) {
-                                    var index = instance._timings.indexOf(match);
-                                    var wordId = instance.getWordId(index);
-                                    instance._removeHighlight();
-                                    instance._addHighlight(wordId);
-                                });
-                            };
-                        }
-                        prevPosition = position;
-                    };
-                    createjs.Ticker.addEventListener("tick", instance._listener);
-                    instance._audioInstance.object.on("complete", function() {
-                        instance._cleanupHighlight();
-                    });
-                }
+            if (this._isPaused) {
+                instance._resume(action);
             } else {
-                // TODO: By calculating the delta time.
+                this._isPlaying = true;
+                var prevPosition = 0;
+                if (audio) {
+                    this._playAudio(audio);
+                    this._listener = function() {
+                        instance._position.current = Number(instance._audioInstance.object.position.toFixed(0));
+                        instance._highlight();
+                        instance._position.previous = instance._position.current;
+                    };
+                } else {
+                    this._time = Date.now();
+                    this._listener = function() {
+                        if (!instance._isPaused) {
+                            instance._position.current = Date.now() - instance._time + instance._position.pause;
+                            instance._highlight();
+                            if (instance._position.previous > instance._timings[instance._timings.length -1] + 500) {
+                                instance._cleanupHighlight();
+                            }
+                            instance._position.previous = instance._position.current;
+                        }
+                    }
+                }
+                createjs.Ticker.addEventListener("tick", instance._listener);
             }
         } else {
             console.info("No timing data to play highlight text:", this._id);
         }
     },
     pause: function(action) {
-        var instance = this;
-        var audio = action.audio || this._data.audio;
-        if (this._timings.length > 0) {
-            if (audio) {
-                instance._isPlaying = false;
-                AudioManager.pause({asset: audio}, instance._audioInstance);
+        if (this._isPlaying) {
+            var instance = this;
+            var audio = action.audio || this._data.audio;
+            if (this._timings.length > 0) {
+                instance._isPaused = true;
+                if (audio) {
+                    AudioManager.pause({asset: audio}, instance._audioInstance);
+                } else {
+                    instance._position.pause = instance._position.current;
+                }
             } else {
-                // TODO: By calculating the delta time.
+                console.info("No timing data:", this._id);
             }
         } else {
-            console.info("No timing data:", this._id);
+            console.info("highlight is not playing to pause:", this._id);
         }
-
     },
     togglePlay: function(action) {
-        if (this._isPlaying) {
+        if (this._isPlaying && !this._isPaused) {
             this.pause(action);
         } else {
             this.play(action);
         }
     },
-    resume: function(action) {
+    _resume: function(action) {
         var instance = this;
         var audio = action.audio || this._data.audio;
         if (this._timings.length > 0) {
+            instance._isPaused = false;
             if (audio) {
-                instance._isPlaying = true;
                 AudioManager.play({asset: audio, stageId: instance._stage._id}, instance._audioInstance);
             } else {
-                // TODO: By calculating the delta time.
+                instance._time = Date.now();
             }
         } else {
             console.info("No timing data:", this._id);
@@ -128,12 +130,34 @@ var HighlightTextPlugin = HTMLPlugin.extend({
         if (this._timings.length > 0) {
             if (audio) {
                 AudioManager.stop({asset: audio, stageId: instance._stage._id});
-                instance._cleanupHighlight();
-            } else {
-                // TODO: By calculating the delta time.
             }
+            instance._cleanupHighlight();
         } else {
             console.info("No timing data:", this._id);
+        }
+    },
+    _playAudio: function(audio) {
+        var instance = this;
+        instance._data.audio = audio;
+        instance._audioInstance = AudioManager.play({asset: audio, stageId: this._stage._id});
+        instance._audioInstance.object.on("complete", function() {
+            instance._cleanupHighlight();
+        });
+    },
+    _highlight: function() {
+        var instance = this;
+        if (instance._position.current && instance._isPlaying) {
+            var matches = _.filter(instance._timings, function(time) {
+                return (time >= instance._position.previous && time < instance._position.current);
+            });
+            if (matches.length >0) {
+                _.each(matches, function(match) {
+                    var index = instance._timings.indexOf(match);
+                    var wordId = instance.getWordId(index);
+                    instance._removeHighlight();
+                    instance._addHighlight(wordId);
+                });
+            };
         }
     },
     _cleanupHighlight: function() {
@@ -141,6 +165,12 @@ var HighlightTextPlugin = HTMLPlugin.extend({
         this._removeHighlight();
         if (this._listener) createjs.Ticker.removeEventListener("tick", this._listener);
         if (this._audioInstance) this._audioInstance = undefined;
+        this._time = 0;
+        this._position = {
+            previous: 0,
+            current: 0,
+            pause: 0
+        };
     },
     _removeHighlight: function() {
         jQuery("."+this._wordClass).css({"background-color": "none", "padding": "0px", "font-weight": "normal"});
