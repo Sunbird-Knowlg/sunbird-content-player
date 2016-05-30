35c4f5c3b56a7ebf8c83287655f434d3abdcc0dc
Mahesh <mahesh@canopusconsulting.com>
2016-05-30 09:11:44 +0530

    highlight-text plugin jasmine test cases.

--- player/public/js/test/HighlightTextPluginSpec.js
+++ player/public/js/test/HighlightTextPluginSpec.js
@@ -65,15 +65,53 @@ describe('Highlight Text Plugin test cases', function() {
         }, 100);
     });
 
-    xit('Playing highlight-text with audio', function() {
-        expect(true).toBe(true);
+    it('Playing highlight-text with audio', function(done) {
+        var ht1 = PluginManager.getPluginObject("ht1");
+        ht1.play({});
+        expect(ht1._isPlaying).toEqual(true);
+        expect(ht1._isPaused).toEqual(false);
+        setTimeout(function() {
+            expect(ht1._time > 0).toEqual(true);
+            expect(ht1._position.previous > 0).toEqual(true);
+            expect(ht1._position.current > 0).toEqual(true);
+            expect(ht1._position.pause == 0).toEqual(true);
+            expect(ht1._listener).toBeDefined();
+            done();
+        }, 100);
     });
 
-    xit('Pause highlight-text with audio', function() {
-        expect(true).toBe(true);
+    it('Pause highlight-text with audio', function(done) {
+        var ht1 = PluginManager.getPluginObject("ht1");
+        ht1.play({});
+        expect(ht1._isPlaying).toEqual(true);
+        expect(ht1._isPaused).toEqual(false);
+        setTimeout(function() {
+            expect(ht1._time > 0).toEqual(true);
+            expect(ht1._position.previous > 0).toEqual(true);
+            expect(ht1._position.current > 0).toEqual(true);
+            expect(ht1._position.pause == 0).toEqual(true);
+            expect(ht1._listener).toBeDefined();
+            ht1.pause({});
+            expect(ht1._isPaused).toEqual(true);
+            done();
+        }, 100);
     });
 
-    xit('Stop highlight-text with audio', function() {
-        expect(true).toBe(true);
+    it('Stop highlight-text with audio', function(done) {
+        var ht1 = PluginManager.getPluginObject("ht1");
+        ht1.play({});
+        expect(ht1._isPlaying).toEqual(true);
+        expect(ht1._isPaused).toEqual(false);
+        setTimeout(function() {
+            expect(ht1._time > 0).toEqual(true);
+            expect(ht1._position.previous > 0).toEqual(true);
+            expect(ht1._position.current > 0).toEqual(true);
+            expect(ht1._position.pause == 0).toEqual(true);
+            expect(ht1._listener).toBeDefined();
+            ht1.stop({});
+            expect(ht1._isPlaying).toEqual(false);
+            expect(ht1._isPaused).toEqual(false);
+            done();
+        }, 100);
     });
 });
