26ce4bd2ac88a36f6950f486614e76a250307d10
Mahesh <mahesh@canopusconsulting.com>
2016-06-01 16:23:21 +0530

    Scribble pad issues with grouping

--- js-libs/renderer/plugin/ScribblePlugin.js
+++ js-libs/renderer/plugin/ScribblePlugin.js
@@ -1,46 +1,59 @@
 var ScribblePlugin = Plugin.extend({
     _type: 'scribble',
     _render: true,
-    _container: undefined,
+    _isContainer: true,
     _data: undefined,
     _oldPt: undefined,
     _oldMidPt: undefined,
-    _shapePoint: undefined,
-    _shapeEndPoint: undefined,
+    _startPoint: undefined,
+    _endPoint: undefined,
     initPlugin: function(data) {
         this._data = data;
         var color = data.color || "#000";
         var background = data.fill || "#fff"
         var dims = this.relativeDims();
-        var container = new createjs.Container();
+        this._self = new createjs.Container();
+        this._self.x = dims.x;
+        this._self.y = dims.y;
         var bgShape = new createjs.Shape();
 
         // addEventListener for container
-        container.addEventListener("mousedown", this.handleMouseDown.bind(this), true);
+        this._self.addEventListener("mousedown", this.handleMouseDown.bind(this), true);
 
-        container.addEventListener("pressup", this.handleMouseUp.bind(this), true);
+        this._self.addEventListener("pressup", this.handleMouseUp.bind(this), true);
         createjs.Ticker.setFPS(50);
 
-
+        // create paint brush
         this.paintBrush = new createjs.Shape();
-        bgShape.graphics = new createjs.Graphics().beginFill(background).drawRect(dims.x, dims.y, dims.w, dims.h);
+        this.paintBrush.x = 0;
+        this.paintBrush.y = 0;
+        this._self.addChild(this.paintBrush);
+
+        bgShape.graphics = new createjs.Graphics().beginFill(background).drawRect(0, 0, dims.w, dims.h);
         bgShape.alpha = data.opacity || 1;
-        container.addChild(bgShape);
-        container.addChild(this.paintBrush);
-        this._self = container;
+        this._self.addChild(bgShape);
+        
+        var shapeData = {"shape":{"type":"rect","x":0,"y":0,"w":100,"h":100}};
+        if (data.fill) shapeData.shape.fill = data.fill;
+        if (data.stroke) shapeData.shape.stroke = data.stroke;
+        if (data["stroke-width"]) shapeData.shape["stroke-width"] = data["stroke-width"];
+        this.invokeChildren(shapeData, this, this._stage, this._theme);
+        this.setBounderies();
+    },
+    setBounderies: function() {
+        var dims = this.relativeDims();
+        var x = dims.x + dims.w - 5;
+        var y = dims.y + dims.h - 5;
+        this._endPoint = new createjs.Point(x, y);
+        this._startPoint = new createjs.Point(dims.x + 5, dims.y + 5);
     },
     handleMouseDown: function(event) {
         if (!event.primary) {
             return;
         }
-
         var mousePoint = Renderer.theme.mousePoint();
+        mousePoint = this._self.globalToLocal(mousePoint.x, mousePoint.y);
         this._oldPt = new createjs.Point(mousePoint.x, mousePoint.y);
-        var dims = this.relativeDims();
-        var x = dims.x + dims.w - 5;
-        var y = dims.y + dims.h - 5;
-        this._shapeEndPoint = new createjs.Point(x, y);
-        this._shapePoint = new createjs.Point(dims.x + 5, dims.y + 5);
         this._self.addEventListener("pressmove", this.handleMouseMove.bind(this), true);
     },
     handleMouseMove: function(event) {
@@ -48,15 +61,12 @@ var ScribblePlugin = Plugin.extend({
             return;
         }
         var mousePoint = Renderer.theme.mousePoint();
+        mousePoint = this._self.globalToLocal(mousePoint.x, mousePoint.y);
         var thickness = this.isInt(this._data.thickness) ?  this._data.thickness : 3;
-        if (((event.stageX > this._shapePoint.x) && (event.stageX < this._shapeEndPoint.x)) && ((event.stageY > this._shapePoint.y) && (event.stageY < this._shapeEndPoint.y))) {
+        if (((event.stageX > this._startPoint.x) && (event.stageX < this._endPoint.x)) && ((event.stageY > this._startPoint.y) && (event.stageY < this._endPoint.y))) {
             this.paintBrush.graphics.setStrokeStyle(thickness, 'round').beginStroke(this._data.color || "#000");
-
-            if (((event.stageX > this._shapePoint.x) && (event.stageX < this._shapeEndPoint.x)) && ((event.stageY > this._shapePoint.y) && (event.stageY < this._shapeEndPoint.y))) {
-                this.paintBrush.graphics.mt(this._oldPt.x, this._oldPt.y).lineTo(mousePoint.x + 1, mousePoint.y + 1);
-                this._oldPt = { x: mousePoint.x, y: mousePoint.y };
-            }
-
+            this.paintBrush.graphics.mt(this._oldPt.x, this._oldPt.y).lineTo(mousePoint.x + 1, mousePoint.y + 1);
+            this._oldPt = { x: mousePoint.x, y: mousePoint.y };
         }
         Renderer.update = true;
     },
@@ -73,6 +83,7 @@ var ScribblePlugin = Plugin.extend({
     isInt : function(value) {
       var x = parseFloat(value);
       return !isNaN(value) && (x | 0) === x;
-    }
+    },
+    drawBorder : function() {}
 });
 PluginManager.registerPlugin('scribble', ScribblePlugin);
