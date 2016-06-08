c1cffa390aec96fc15cff5a6dda43db374505d5e
Mahesh <mahesh@canopusconsulting.com>
2016-06-08 17:35:46 +0530

    removing unused files.

--- js-libs/renderer/plugin/karaokePlugin.js
+++
@@ -1,8 +0,0 @@
-var KaraokePlugin = Plugin.extend({
-	_type: 'image',
-	_isContainer: false,
-	_render: true,
-	initPlugin: function(data) {
-	}
-});
-PluginManager.registerPlugin('karaoke', KaraokePlugin);
