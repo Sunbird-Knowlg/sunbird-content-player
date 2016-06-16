26c61a2e62374e02c18b539b7d51aec5d1789a7d
vinukumar-vs <vinukumar_vs@yahoo.com>
2016-06-16 20:54:54 +0530

    Removed stage clear, because on stage change it's showing white screen.

--- js-libs/renderer/plugin/ThemePlugin.js
+++ js-libs/renderer/plugin/ThemePlugin.js
@@ -154,8 +154,6 @@ var ThemePlugin = Plugin.extend({
         childPlugin.setIndex(nextIdx);
     },
     replaceStage: function(stageId, effect) {
-        this._self.clear();
-        this._self.removeAllChildren();
         this.disableInputs();
         this.inputs = [];
         this.removeHtmlElements();
