ac77c2dcd5371b6d18383f5d4f984e43a0e40228
manjudr <manju143dr@gmail.com>
2016-10-06 15:03:53 +0530

    #583 added the old reset code

--- js-libs/renderer/command/ResetCommand.js
+++ js-libs/renderer/command/ResetCommand.js
@@ -2,7 +2,12 @@ var ResetCommand = Command.extend({
 	_name: 'RESET',
 	_isPluginAction: false,
 	invoke: function(action) {
-		ControllerManager.resetController(action.controller);
+		var c = ControllerManager.instanceMap[action.cType+"."+action.controller];
+        if ("undefined" != typeof c) {
+            c.reset();
+        } else {
+            console.warn("No controller find with id:", action.controller);
+        }
 	}
 });
 CommandManager.registerCommand('RESET', ResetCommand);
