7e6b4c3ecabacb505a0dd71c83847d6e6393154a
vinukumar-vs <vinukumar_vs@yahoo.com>
2016-11-10 15:41:56 +0530

    New S3(Mumbai) URL & Api changes

--- ansible1/qa/AppConfig.js
+++ ansible1/qa/AppConfig.js
@@ -3,11 +3,11 @@ AppConfig = {
 	PROCESSING_TIMEOUT: 10, // in minutes
 	APP_STATUS: "AT",
 	App_RUNTIME: "AT",
-	QA: "//qa.ekstep.in/api",
 	dev: "//dev1.ekstep.in/api",
+	QA: "//qa1.ekstep.in/api",
 	production: "//community.ekstep.in/api",
 	recorder: "AUDIO_RECORDER",
-	flavor: "DEPLOYMENT"
+	flavor: "QA"
 }
 
 
