a16f5052e5d40d0a608874ac1e6f88c0a88d38e9
krushanu <mohapatrakrushanu@gmail.com>
2016-11-18 15:31:19 +0530

    #824 EBM.js added

---
+++ js-libs/renderer/manager/EventBusManager.js
@@ -0,0 +1,20 @@
+EventBusManager = {
+	init: finction() {
+
+	}
+	// addEventListener: function(evtName, callback, scope) {
+  //       EventBus.addEventListener(evtName, callback, scope);
+  //   },
+  //   dispatch: function(evtName, target, data) {
+  //       EventBus.dispatch(evtName, target, data);
+  //   },
+  //   removeEventListener: function(evtName, callback, scope) {
+  //       EventBus.removeEventListener(evtName, callback, scope)
+  //   },
+  //   hasEventListener: function(evtName, callback, scope) {
+  //       EventBus.hasEventListener(type, callback, scope)
+  //   },
+  //   getEvents: function() {
+  //       //return EventBus.getEvents();
+  //   }
+}
