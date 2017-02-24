0a3b25287618223cbf48c292dbdf700652e168e3
akashgupta9990 <aakash.gupta@tarento.com>
2017-02-24 12:58:15 +0530

    Issue #1068 fix:changed appconfig from dev1.ekstep.in to dev.ekstep.in

--- player/public/js/app/AppConfig.js
+++ player/public/js/app/AppConfig.js
@@ -4,7 +4,7 @@ AppConfig = {
 	APP_STATUS: "AT",
 	App_RUNTIME: "AT",
 	QA: "//qa.ekstep.in/api",
-	dev: "//dev1.ekstep.in/api",
+	dev: "//dev.ekstep.in/api",
 	production: "//community.ekstep.in/api",
 	recorder: "AUDIO_RECORDER",
 	flavor: "DEPLOYMENT",
