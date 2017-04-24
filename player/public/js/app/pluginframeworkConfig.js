84cce70e597649189ed4a282069770a1f9e32bff
manjudr <manju143dr@gmail.com>
2017-04-25 00:17:47 +0530

    Issue #1166 feat:PluginFramework config setup

---
+++ player/public/js/app/pluginframeworkConfig.js
@@ -0,0 +1,19 @@
+/*console.info("Plugin framework Config is ready");
+var pfConfig = {
+    async: async,
+    repos: [org.ekstep.pluginframework.publishedRepo],
+    pluginsPath: "/widgets/content-plugins",
+    env: 'renderer',
+    corePlugins : 
+};*/
+var pfConfig = {},
+    initializePluginFramwork = function(gameRelPath) {
+    	console.log("Plugin framework init is started..");
+        var pluginsPath = isbrowserpreview ? "/content-plugins" : "/widgets/content-plugins";
+        pfConfig.pluginsPath = pluginsPath;
+        pfConfig.env = 'renderer';
+        pfConfig.async = async;
+        pfConfig.pluginRepo = gameRelPath + pfConfig.pluginsPath;
+        pfConfig.backwardCompatibalityURL = gameRelPath + '/widgets/';
+        org.ekstep.pluginframework.initialize(pfConfig);
+    };
