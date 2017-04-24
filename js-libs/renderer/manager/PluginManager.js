d53209c60a1becfe1be025aed4463830e773a74d
manjudr <manju143dr@gmail.com>
2017-04-24 14:39:53 +0530

    Issue #1168 feat: PluginManager to invoke the corePlugins and customplguins

--- js-libs/renderer/manager/PluginManager.js
+++ js-libs/renderer/manager/PluginManager.js
@@ -1,8 +1,4 @@
 PluginManager = {
-    pluginMap: {},
-    customJSLibMap: {},
-    pluginObjMap: {},
-    errors: [],
     defaultResWidth: 1920,
     defaultResHeight: 1200,
     registerPlugin: function(id, plugin) {
@@ -13,31 +9,31 @@ PluginManager = {
         return org.ekstep.pluginframework.pluginManager.isDefined(id);
     },
     invoke: function(id, data, parent, stage, theme) {
-        if (this.isCanvasCorePlugin(id)) {
-            return this.invokeCorePlugin(id, data, parent, stage, theme);
+        var plugin;
+        if (this.isPlugin(id)) {
+            if (this.isCanvasCorePlugin(id)) {
+                // TODO: Will remove this check once the pluginmanifest is added to canvas coreplugins
+                plugin = org.ekstep.pluginframework.pluginManager.plugins[id];
+                return this.invokePlugins(plugin, data, parent, stage, theme);
+            } else {
+                plugin = org.ekstep.pluginframework.pluginManager.plugins[id].p;
+                return this.invokePlugins(plugin, data, parent, stage, theme);
+            }
         } else {
-            //return this.invokeCorePlugin(id, data, parent, stage, theme);
-            org.ekstep.pluginframework.pluginManager.invoke(id, data, parent, undefined, stage, theme);
+            console.warn("Plugin not found", p);
         }
     },
-    // TODO : Will remove this function once the manifest added to each core plugins
-    invokeCorePlugin: function(id, data, parent, stage, theme) {
+    invokePlugins: function(plugin, data, parent, stage, theme) {
         var p;
-        var plugin = org.ekstep.pluginframework.pluginManager.plugins[id];
-        if (!plugin) {
-            console.warn("Plugin not found");
+        if (_.isArray(data)) {
+            data.forEach(function(d) {
+                p = new plugin(d, parent, stage, theme);
+            });
         } else {
-            if (_.isArray(data)) {
-                data.forEach(function(d) {
-                    p = new plugin(d, parent, stage, theme);
-                });
-            } else {
-                p = new plugin(data, parent, stage, theme);
-            }
+            p = new plugin(data, parent, stage, theme);
         }
         return p;
     },
-
     isCanvasCorePlugin: function(id) {
         corePlugin = ['set', 'tween', 'video', 'shape', 'sprite', 'summary', 'testcase', 'text', 'input', 'mcq', 'mtf', 'option', 'options', 'placeholder', 'scribble', 'theme', 'stage', 'audio', 'g', 'div', 'embed', 'grid', 'htext', 'hotspot', 'image', ];
         return _.contains(corePlugin, id);
