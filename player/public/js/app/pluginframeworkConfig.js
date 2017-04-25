79d14ff9041750c7f7cf29ae5260a3053d6a4f25
manjudr <manju143dr@gmail.com>
2017-04-25 16:38:00 +0530

    Issue #1166 feat:Framework config changes.

--- player/public/js/app/pluginframeworkConfig.js
+++ player/public/js/app/pluginframeworkConfig.js
@@ -6,14 +6,15 @@ var pfConfig = {
     env: 'renderer',
     corePlugins : 
 };*/
-var pfConfig = {},
-    initializePluginFramwork = function(gameRelPath) {
-    	console.log("Plugin framework init is started..");
-        var pluginsPath = isbrowserpreview ? "/content-plugins" : "/widgets/content-plugins";
-        pfConfig.pluginsPath = pluginsPath;
-        pfConfig.env = 'renderer';
-        pfConfig.async = async;
-        pfConfig.pluginRepo = gameRelPath + pfConfig.pluginsPath;
-        pfConfig.backwardCompatibalityURL = gameRelPath + '/widgets/';
-        org.ekstep.pluginframework.initialize(pfConfig);
-    };
+var pfConfig = {};
+var initializePluginFramwork = function(gameRelPath) {
+    console.log("Plugin framework init is started..");
+    // Note : isbrowserpreview will be true only for the portal and editor.
+    var pluginsPath = isbrowserpreview ? "/content-plugins" : "/widgets/content-plugins";
+    pfConfig.pluginsPath = pluginsPath;
+    pfConfig.env = 'renderer';
+    pfConfig.async = async;
+    pfConfig.pluginRepo = gameRelPath + pfConfig.pluginsPath;
+    pfConfig.backwardCompatibalityURL = gameRelPath + '/widgets/';
+    org.ekstep.pluginframework.initialize(pfConfig);
+};
