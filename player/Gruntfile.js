7fd69089c450c73c0c00c5c359f34da586d968bc
akashgupta9990 <aakash.gupta@tarento.com>
2017-05-05 15:50:00 +0530

    Issue #1191 fix : added CryptoJS/md5 file in gruntfile and removed from html files

--- player/Gruntfile.js
+++ player/Gruntfile.js
@@ -53,6 +53,7 @@ module.exports = function(grunt) {
                         'public/js/thirdparty/exclude/creatine-1.0.0.min.js',
                         'public/js/thirdparty/exclude/eventbus.min.js',
                         'public/js/thirdparty/exclude/Class.js',
+                        'public/js/thirdparty/exclude/md5.js',
                         'public/js/app/genieservices.js',
                         'public/js/app/EkstepRendererApi.js',
                         'public/js/app/renderer.js'
