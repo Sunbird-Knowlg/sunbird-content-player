2c9e12a1116a555e1b52fe2b0a42e374239da269
akashgupta9990 <aakash.gupta@tarento.com>
2017-05-16 14:11:39 +0530

    Issue #1210 fix: removed old_content logic

--- player/public/js/app/app.js
+++ player/public/js/app/app.js
@@ -783,12 +783,10 @@ angular.module('genie-canvas', ['ionic', 'ngCordova', 'genie-canvas.services'])
         }
 
         $scope.init = function() {
-            if (_.isUndefined($rootScope.content) || !_.isEqual($rootScope.content, localStorageGC.getItem('content_old'))) {
-                localStorageGC.removeItem('content_old');
+            if (_.isUndefined($rootScope.content)) {
                 localStorageGC.update();
                 // Updating the current content object by getting from localStage
-                var oldContent = localStorageGC.getItem('content_old');
-                content = !_.isEmpty(oldContent) ? oldContent : localStorageGC.getItem('content');
+                content = localStorageGC.getItem('content');
                 $rootScope.content = content;
             }
             localStorageGC.setItem('content_old',$rootScope.content)
