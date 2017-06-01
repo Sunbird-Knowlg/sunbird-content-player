b71bae92e075f4ac2fb5f25a7ae24d1ae1a68c0f
vinukumar-vs <vinukumar_vs@yahoo.com>
2017-06-01 18:43:01 +0530

    Issue #1224 chore: App config changes for pord, qa & dev. Also code clean.

--- ansible/prod/AppConfig.js
+++ ansible/prod/AppConfig.js
@@ -1,19 +1,10 @@
 AppConfig = {
 	version: "BUILD_NUMBER",
 	PROCESSING_TIMEOUT: 10, // in minutes
-	APP_STATUS: "AT",
-	App_RUNTIME: "AT",
-	dev: "//dev.ekstep.in/api",
-	QA: "//qa.ekstep.in/api",
 	production: "//api.ekstep.in",
 	recorder: "AUDIO_RECORDER",
 	flavor: "production",
-	S3_content_host: "/assets/public/content/",
+	S3_CONTENT_HOST: "/assets/public/content/",
 	PREVIEW_PLUGINSPATH: "/content-plugins",
-	DEVICE_PLUGINSPATH: "/widgets/content-plugins",
-	telemetryApiAuth: {"username": "ZWtzdGVw", "password": "Z2VuaWU0Mg=="}
+	DEVICE_PLUGINSPATH: "/widgets/content-plugins"
 }
-
-
-
-
--- player/public/js/app/AppConfig.js
+++ player/public/js/app/AppConfig.js
@@ -1,14 +1,13 @@
 AppConfig = {
 	version: "BUILD_NUMBER",
 	PROCESSING_TIMEOUT: 10, // in minutes
-	APP_STATUS: "AT",
-	App_RUNTIME: "AT",
 	dev: "//dev.ekstep.in/api",
 	QA: "//qa.ekstep.in/api",
 	production: "//api.ekstep.in",
+	sandbox: "//sandbox.ekstep.in/api",
 	recorder: "AUDIO_RECORDER",
 	flavor: "DEPLOYMENT",
-	S3_content_host: "/assets/public/content/",
+	S3_CONTENT_HOST: "/assets/public/content/",
 	PREVIEW_PLUGINSPATH: "/content-plugins",
 	DEVICE_PLUGINSPATH: "/widgets/content-plugins",
 	telemetryApiAuth: {"username": "ZWtzdGVw", "password": "czNjcjN0Mw=="}
