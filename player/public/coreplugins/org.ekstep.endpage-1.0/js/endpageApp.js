893c69c3349eb0cc0b706a86a3c37386f38c0d16
manjudr <manju143dr@gmail.com>
2017-06-22 17:46:26 +0530

    Issue #1279 feat: Endpage plugin chagens

--- player/public/coreplugins/org.ekstep.endpage-1.0/js/endpageApp.js
+++ player/public/coreplugins/org.ekstep.endpage-1.0/js/endpageApp.js
@@ -1,6 +1,4 @@
 var endPageApp = angular.module("endPageApp", []);
-
-
 endPageApp.controller("endPageController", function($scope, $rootScope, $state, $stateParams) {
     console.info("i am controller endpage");
     $scope.showFeedbackArea = true;
@@ -14,7 +12,6 @@ endPageApp.controller("endPageController", function($scope, $rootScope, $state, 
     $scope.creditsBody = '<div class="gc-popup-new credit-popup"><div class="gc-popup-title-new"> {{languageSupport.credit}}</div> <div class="gc-popup-body-new"><div class="font-lato credit-body-icon-font"><div class="content-noCredits" ng-show="content.imageCredits == null && content.voiceCredits == null && content.soundCredits == null">{{languageSupport.noCreditsAvailable}}</div><table style="width:100%; table-layout: fixed;"><tr ng-hide="content.imageCredits==null"><td class="credits-title">{{languageSupport.image}}</td><td class="credits-data">{{content.imageCredits}}</td></tr><tr ng-hide="content.voiceCredits==null"><td class="credits-title">{{languageSupport.voice}}</td><td class="credits-data">{{content.voiceCredits}}</td></tr><tr ng-hide="content.soundCredits==null"><td class="credits-title">{{languageSupport.audio}}</td><td class="credits-data">{{content.soundCredits}}</td></tr></table></div></div></div>';
   	$rootScope.imageBasePath = "assets/icons/";
   	$rootScope.languageSupport = AppLables;
-  	
     $scope.arrayToString = function(array) {
         return (_.isString(array)) ? array : (!_.isEmpty(array) && _.isArray(array)) ? array.join(", ") : "";
     };
@@ -24,7 +21,6 @@ endPageApp.controller("endPageController", function($scope, $rootScope, $state, 
         });
         EventBus.dispatch("event:openUserSwitchingModal");
     }
-
     $scope.setCredits = function(key) {
         if ($scope.content[key]) {
             $scope.content[key] = $scope.arrayToString($scope.content[key]);
@@ -32,21 +28,20 @@ endPageApp.controller("endPageController", function($scope, $rootScope, $state, 
             $scope.content[key] = null;
         }
     };
-
-    $scope.showCredits = function(key) {
-        if ($scope.content.imageCredits == null && $scope.content.voiceCredits == null && $scope.content.soundCredits == null) {
-            console.warn("No metadata imageCredits,voiceCredites and soundCredits");
+        $scope.showCredits = function(key) {
+            if ($scope.content.imageCredits == null && $scope.content.voiceCredits == null && $scope.content.soundCredits == null) {
+                console.warn("No metadata imageCredits,voiceCredites and soundCredits");
+            }
+            jQuery("#creditsPopup").show();
+            TelemetryService.interact("TOUCH", "gc_credit", "TOUCH", {
+                stageId: "ContentApp-CreditsScreen",
+                subtype: "ContentID"
+            });
         }
-        jQuery("#creditsPopup").show();
-        TelemetryService.interact("TOUCH", "gc_credit", "TOUCH", {
-            stageId: "ContentApp-CreditsScreen",
-            subtype: "ContentID"
-        });
-    }
 
     $scope.ep_replayContent = function() {
+        console.info("yes replay")
         $rootScope.replayContent();
-        //Resetting mute state
         var muteElement = document.getElementById("unmute_id");
         if (!_.isNull(muteElement)) {
             muteElement.style.display = "none";
@@ -132,7 +127,7 @@ endPageApp.controller("endPageController", function($scope, $rootScope, $state, 
         $scope.enableFeedbackSubmit();
     }
 
-    $scope.init = function() {
+    $scope.handleEndpage = function() {
         if (_.isUndefined($rootScope.content)) {
             localStorageGC.update();
             // Updating the current content object by getting from localStage
@@ -184,15 +179,167 @@ endPageApp.controller("endPageController", function($scope, $rootScope, $state, 
         jQuery('#gcFbPopup').removeClass('gc-fc-popup-keyboard');
     }
 
-    setTimeout(function() {
-        $scope.init();
-    }, 0);
-
-    $rootScope.$on('loadEndPage', function() {
+    
+    $scope.initEndpage = function() {
+        $scope.hideCanvas();
+        $scope.handleEndpage();
         if (_.isUndefined($rootScope.content)) {
-            $scope.init();
+            localStorageGC.update();
+           content = localStorageGC.getItem('content');
+            $rootScope.content = content;
         }
-    });
+    };
+    $scope.hideCanvas = function(){
+        jQuery('#gameCanvas').css({
+            display: 'none'
+        });
+        jQuery('#overlay').css({display: 'none'})
+    };
+    $scope.initEndpage();
+    // $rootScope.$on('loadEndPage', function() {
+    //     if (_.isUndefined($rootScope.content)) {
+    //         $scope.init();
+    //     }
+    // });
+});
+endPageApp.controller('RelatedContentCtrl', function($scope, $rootScope, $state, $stateParams) {
+        $scope.showRelatedContent = false;
+        $scope.contentShowMore = false;
+        $scope.showRelatedContentHeader = true;
+        $scope.relatedContents = [];
+        $scope.relatedContentPath = [];
+        $scope.collectionTree = undefined;
+        $scope.playRelatedContent = function(content, index) {
+            var contentId = [];
+            collectionPath = $scope.relatedContentPath;
+            var eleId = "gc_nextcontent";
+            var values = [];
+            var contentIds = [];
+            // Send only for normal contet/ content played directly from Genie
+            if (_.isUndefined($scope.collectionTree) || _.isEmpty($scope.collectionTree)) {
+                if ($scope.relatedContents.length > 0) {
+                    contentIds = _.pluck($scope.relatedContents, 'identifier');
+                }
+                eleId = "gc_relatedcontent";
+                values = [{
+                    PositionClicked: index + 1
+                }, {
+                    ContentIDsDisplayed: contentIds,
+                }, {
+                    id: $scope.relatedContentItem ? $scope.relatedContentItem.params.resmsgid : "",
+                    type: $scope.relatedContentItem ? $scope.relatedContentItem.id : ''
+                }]
+            }
+            TelemetryService.interact("TOUCH", eleId, "TOUCH", {
+                stageId: "endpage",
+                subtype: "",
+                values: values
+            });
+            TelemetryService.end();
+            GlobalContext.game.id = content.identifier
+            GlobalContext.game.pkgVersion = content.pkgVersion;
+            var contentExtras = [];
+            if (!(_.isUndefined($scope.collectionTree) || _.isEmpty($scope.collectionTree))) {
+                // is a collection
+                _.each($scope.relatedContentPath, function(eachObj) {
+                    contentExtras.push(_.pick(eachObj, 'identifier', 'contentType'));
+                });
+            }
+            // Check is content is downloaded or not in Genie.
+            org.ekstep.service.content.getContentAvailability(content.identifier)
+                .then(function(contetnIsAvailable) {
+                    if (contetnIsAvailable) {
+                        // This is required to setup current content details which is going to play
+                        org.ekstep.contentrenderer.getContentMetadata(content.identifier, function() {
+                            if ($scope.collectionTree) {
+                                GlobalContext.game.contentExtras = contentExtras;
+                                localStorageGC.setItem("contentExtras", GlobalContext.game.contentExtras);
+                            }
+                            $state.go('playContent', {
+                                'itemId': content.identifier
+                            });
+                        });
+                    } else {
+                        $scope.navigateToDownloadPage(contentExtras, content.identifier);
+                    }
+                })
+                .catch(function(err) {
+                    console.info("contentNotAvailable : ", err);
+                    $scope.navigateToDownloadPage(contentExtras, content.identifier);
+                });
+        }
+        $scope.navigateToDownloadPage = function(contentExtras, contentId) {
+            // stringify contentExtras array to string
+            var deepLinkURL = "ekstep://c/" + contentId;
+            if (!_.isEmpty(contentExtras)) {
+                contentExtras.pop();
+                contentExtras = JSON.stringify(contentExtras);
+                deepLinkURL += "&contentExtras=" + contentExtras;
+            }
+            console.log("deepLinkURL: ", deepLinkURL);
+            window.open(deepLinkURL, "_system");
+        }
+        $scope.getRelatedContent = function(list) {
+            org.ekstep.service.content.getRelatedContent(TelemetryService._user.uid, list)
+                .then(function(item) {
+                    if (!_.isEmpty(item)) {
+                        $scope.relatedContentItem = item;
+                        var list = [];
+                        if (!_.isEmpty(item.collection)) {
+                            $scope.showRelatedContent = true;
+                            $scope.relatedContentPath = item.collection;
+                            list = [item.collection[item.collection.length - 1]];
+                            list[0].appIcon = list[0].path + '/' + list[0].appIcon;
+                        } else if (!_.isEmpty(item.content)) {
+                            $scope.showRelatedContent = true;
+                            $scope.contentShowMore = true;
+                            list = _.first(_.isArray(item.content) ? item.content : [item.content], 2);
+                        }
+
+                        if (!_.isEmpty(list)) {
+                            $scope.$apply(function() {
+                                $scope.relatedContents = list;
+                                jQuery('#endPageLoader').hide();
+                            });
+                        } else {
+                            $scope.showRelatedContentHeader = false;
+                            jQuery('#endPageLoader').hide();
+                        }
+                    }
+                })
+        }
+
+        $scope.renderRelatedContent = function(id) {
+            var list = [];
+            if (_.isUndefined($scope.collectionTree) || _.isEmpty($scope.collectionTree)) {
+                if (("undefined" != typeof cordova)) {
+                    list = [{
+                        "identifier": id,
+                        "mediaType": "Content"
+                    }]
+                    $scope.getRelatedContent(list);
+                }
+            } else {
+                list = $scope.collectionTree;
+                $scope.getRelatedContent(list);
+            }
+        }
+
+        $scope.init = function() {
+            $scope.collectionTree = localStorageGC.getItem('contentExtras');
+            // GlobalContext.game.contentExtras = ("string" == typeof(GlobalContext.game.contentExtras)) ? JSON.parse(GlobalContext.game.contentExtras) : GlobalContext.game.contentExtras;
+
+            if ("undefined" != typeof cordova) {
+                $scope.renderRelatedContent($stateParams.contentId);
+            } else {
+                jQuery('#endPageLoader').hide();
+                $scope.showRelatedContentHeader = false;
+            }
+        }
+
+        $scope.$on('getRelatedContentEvent', function(event) {
+            $scope.init();
+        });
 });
 endPageApp.directive('genie', function($rootScope) {
     return {
@@ -221,7 +368,8 @@ endPageApp.directive('restart', function($rootScope, $state, $stateParams) {
         template: '<div ng-click="restartContent()"><img src="{{imageBasePath}}icn_replay.png"/><span> {{languageSupport.replay}} </span></div>',
         link: function(scope) {
             scope.restartContent = function() {
-                $rootScope.replayContent();
+                console.info('replay')
+                /*$rootScope.replayContent();*/
                 //Resetting mute state
                 var muteElement = document.getElementById("unmute_id");
                 if (!_.isNull(muteElement)) {
@@ -234,7 +382,7 @@ endPageApp.directive('restart', function($rootScope, $state, $stateParams) {
         }
     }
 });
-/*endPageApp.directive('starRating', function($rootScope) {
+endPageApp.directive('starRating', function($rootScope) {
     return {
         //reference: http://jsfiddle.net/manishpatil/2fahpk7s/
         scope: {
@@ -279,4 +427,4 @@ endPageApp.directive('restart', function($rootScope, $state, $stateParams) {
             };
         }
     };
-})*/
+})
