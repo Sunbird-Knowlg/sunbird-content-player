54e51358eaaa71d45806dfc6f539b2b3aa8711ea
manjudr <manju143dr@gmail.com>
2017-06-27 12:54:16 +0530

    Issue #1302 feat:HTML Interface Changes and grunt changes

--- player/Gruntfile.js
+++ player/Gruntfile.js
@@ -969,5 +969,5 @@ module.exports = function(grunt) {
     grunt.registerTask('local-preview-build', ['uglify:renderer', 'uglify:speech', 'uglify:telemetry', 'uglify:js','copy:localPreviewFiles', 'copy:localPreviewMinjs', 'aws_s3:uploadLocalPreviewZip', 'clean:localPreview']);
 // ================= /
     grunt.registerTask('new-buildPreview', ['mkdir:all', 'copy:newmain', 'concat:css', 'concat:externaljs', 'concat:telemetry', 'concat:script', 'clean:deletefiles', 'injector:prview', 'rename:preview']);
-    grunt.registerTask('buildapp', ['new-buildPreview', 'clean:after', 'rename:main', 'injector:prview', 'set-platforms', 'add-cordova-plugin-genieservices', 'cordovacli:add_plugins', 'update_custom_plugins', 'add-speech', 'set-android-library', 'set-xwalkshared-library', 'cordovacli:build_android', 'clean:minjs']);
+    grunt.registerTask('buildapp', ['new-buildPreview', 'clean:after', 'rename:main', 'injector:prview', 'set-platforms', 'add-cordova-plugin-genieservices', 'cordovacli:add_plugins','copy:unsigned', 'update_custom_plugins', 'add-speech', 'set-android-library', 'set-xwalkshared-library', 'cordovacli:build_android', 'clean:minjs']);
 };
--- player/public/js/htmlInterface.js
+++ player/public/js/htmlInterface.js
@@ -1,33 +1,35 @@
 /**
  * @author Manjunath Davanam <manjunathd@ilimi.in>
  */
+var html_renderer = function() {};
+window.org.ekstep.htmlrenderer = new html_renderer();
 
-window.org.ekstep.htmlrenderer = window.top;
+window.org.ekstep.renderer.top = window.top;
 /**
  * To navigate to endPage
  */
 org.ekstep.htmlrenderer.showEndPage = function() {
-	org.ekstep.htmlrenderer.EkstepRendererAPI.showEndPage();
+	org.ekstep.renderer.top.EkstepRendererAPI.showEndPage();
 };
 
 
 /**
  * To Access Canvas Renderer API's
  */
-org.ekstep.htmlrenderer.api  = EkstepRendererAPI();
+org.ekstep.htmlrenderer.api  = org.ekstep.renderer.top.EkstepRendererAPI();
 
 /**
  * To enable Overlay
  */
 org.ekstep.htmlrenderer.enableOverlay = function(){
-	org.ekstep.htmlrenderer.api.showOverlay();
+	org.ekstep.renderer.top.api.showOverlay();
 };
 
 /**
  * To disable Overlay
  */
 org.ekstep.htmlrenderer.enableOverlay = function(){
-	org.ekstep.htmlrenderer.api.hideOverlay();
+	org.ekstep.renderer.top.api.hideOverlay();
 };
 
 
@@ -35,23 +37,25 @@ org.ekstep.htmlrenderer.enableOverlay = function(){
  * To Hide the canvas endpage
  */
 org.ekstep.htmlrenderer.hideEndPage = function(){
-	
+
 };
 
 /**
  * To Access HTML Service
  */
-org.ekstep.htmlrenderer.services = org.ekstep.htmlrenderer.org.ekstep.service.html;
+org.ekstep.htmlrenderer.services = org.ekstep.renderer.top.org.ekstep.service.html;
 
 /**
  * To Access Telemetry service
  */
-org.ekstep.htmlrenderer.telemetryService = org.ekstep.htmlrendere.TelemetryService;
+org.ekstep.htmlrenderer.telemetryService = org.ekstep.renderer.top.TelemetryService;
 
 /**
  * To Access Content Metadata
  */
-org.ekstep.htmlrenderer.contentMetadat = org.ekstep.htmlrendere.org.ekstep.contentrenderer.getContentMetadata;
+org.ekstep.htmlrenderer.getcontentMetadata = function(contentId){
+	return org.ekstep.renderer.top.org.ekstep.contentrenderer.getContentMetadata(contentId);
+}
 
 
 
