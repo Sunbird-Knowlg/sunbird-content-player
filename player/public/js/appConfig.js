c10c2e7eb337936c4aafd7b19f286ace15a99e3a
manjudr <manju143dr@gmail.com>
2017-07-03 22:18:37 +0530

    Issue #1271 feat:Repo plugin is a default plugin for the framework to load the plugins

--- player/public/js/appConfig.js
+++ player/public/js/appConfig.js
@@ -9,6 +9,7 @@ AppConfig = {
     DEVICE_PLUGINSPATH: "/widgets/content-plugins",
     CORE_PLUGINSPATH: 'coreplugins',
     apislug: '/action',
+
     MIMETYPES:["application/vnd.ekstep.ecml-archive", "application/vnd.ekstep.html-archive"],
     DEFAULT_METADATA: {"identifier": "org.ekstep.item.sample", "mimeType": "application/vnd.ekstep.ecml-archive", "name": "Content Preview ", "author": "EkStep", "localData": {"name": "Content Preview ", "loadingMessage": "Without requirements or design, programming is the art of adding bugs to an empty text file. ...", "identifier": "org.ekstep.item.sample", "pkgVersion": 1 }, "isAvailable": true, "path": ""},
     CONTENT_LAUNCHERS: [{
@@ -25,7 +26,9 @@ AppConfig = {
     assetbase : 'assets/icons/',    
     DEFAULT_PLUGINS: [
         {id: 'org.ekstep.launcher',ver: 1.0,type: 'plugin'},
-        {id: 'org.ekstep.collection',ver: 1.0,type: 'plugin'}
+        {id: 'org.ekstep.collection',ver: 1.0,type: 'plugin'},
+        {id: 'org.ekstep.repo',ver: 1.0, type: 'plugin'}
+
     ],
     overlay: {
         userSwitcherEnabled: true,
