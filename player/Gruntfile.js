b9cfaca609d572bd2fccd52bb4496acb4913aac9
manjudr <manju143dr@gmail.com>
2017-07-10 16:44:46 +0530

    Issue #1279 fix:Grunt to remove the collection plugin from the www folder

--- player/Gruntfile.js
+++ player/Gruntfile.js
@@ -660,7 +660,7 @@ module.exports = function(grunt) {
 
     //Build web prview
     grunt.registerTask('preview-init-setup', ['mkdir:all', 'uglify:renderermin', 'copy:main', 'concat:css', 'concat:externaljs', 'concat:telemetry', 'concat:script', 'clean:deletefiles', 'injector:prview', 'replace:buildNumber']);
-    grunt.registerTask('build-preview', ['preview-init-setup' ,'rename:preview', 'clean:minhtml', 'copy:toPreview', 'clean:preview']);
+    grunt.registerTask('build-preview', ['clean','preview-init-setup' ,'rename:preview', 'clean:minhtml', 'copy:toPreview', 'clean:preview']);
 
     //Build AAR
     grunt.registerTask('init-setup', ['set-platforms', 'add-cordova-plugin-genieservices']);
