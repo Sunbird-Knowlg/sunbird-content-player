0acf76f5bf62fe5c8318f35501e33396dfe34c4c
akashgupta9990 <aakash.gupta@tarento.com>
2017-07-12 12:24:26 +0530

    Issue #1333 fix: Fix multiple addition of cdata for device

--- player/public/js/main.js
+++ player/public/js/main.js
@@ -204,7 +204,13 @@ function startTelemetry(id, ver, cb) {
         }];
     }
     var otherData = GlobalContext.config.otherData;
-    (!_.isUndefined(otherData) && !_.isUndefined(otherData.cdata)) ? correlationData.push(otherData.cdata) : correlationData.push({"id": CryptoJS.MD5(Math.random().toString()).toString(), "type": "ContentSession"});
+    if (!_.isUndefined(otherData) && !_.isUndefined(otherData.cdata)) {
+        _.each(otherData.cdata, function(cdata) {
+            correlationData.push(cdata);
+        })
+        delete otherData.cdata;
+    }
+    correlationData.push({"id": CryptoJS.MD5(Math.random().toString()).toString(), "type": "ContentSession"});
     TelemetryService.init(GlobalContext.game, GlobalContext.user, correlationData, otherData).then(function(response) {
         var data = {};
         data.mode =  getPreviewMode();
