15fd218dd56da63c943bb61de44245498d6a76b9
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-14 15:48:55 +0530

    Issue #1271 refactor: update build number

--- player/Gruntfile.js
+++ player/Gruntfile.js
@@ -479,7 +479,7 @@ module.exports = function(grunt) {
                 }]
             },
             build_version: {
-                src: ['www/js/appConfig.js', 'www/preview.html', 'www/preview/preview.html'],
+                src: ['www/preview/scripts/renderer.script.min.js', 'www/preview.html', 'www/preview/preview.html'],
                 overwrite: true,
                 replacements: [{
                     from: /BUILD_NUMBER/g,
@@ -662,7 +662,7 @@ module.exports = function(grunt) {
     grunt.registerTask('set-xwalkshared-library', ['copy:customActivity', 'cordovacli:rm_xwalk', 'cordovacli:add_xwalk_shared', 'replace:xwalk_library']);
 
     //Build web prview
-    grunt.registerTask('preview-init-setup', ['mkdir:all', 'uglify:renderermin', 'copy:main', 'replace:build_version', 'concat:css', 'concat:externaljs', 'concat:telemetry', 'concat:script', 'clean:deletefiles', 'injector:prview', 'replace:build_Number']);
+    grunt.registerTask('preview-init-setup', ['mkdir:all', 'uglify:renderermin', 'copy:main', 'concat:css', 'concat:externaljs', 'concat:telemetry', 'concat:script', 'clean:deletefiles', 'injector:prview', 'replace:build_Number']);
     grunt.registerTask('build-preview', ['clean','preview-init-setup' ,'rename:preview', 'clean:minhtml', 'copy:toPreview', 'clean:preview']);
 
     //Build AAR
