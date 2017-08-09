153c5e523ab1d1ab0b90d8b315ea2a2cbd16b6ce
Manoj Chandrashekar <manoj.chandrashekar@tarento.com>
2017-08-09 19:32:20 +0530

    Issue #1357 fix: Fixes for navigation, stageId type

--- player/public/coreplugins/org.ekstep.epubrenderer-1.0/renderer/plugin.js
+++ player/public/coreplugins/org.ekstep.epubrenderer-1.0/renderer/plugin.js
@@ -10,33 +10,32 @@ org.ekstep.contentrenderer.baseLauncher.extend({
     start: undefined,
     currentPage: 1,
     totalPages: 0,
-    progress: 0,
-    lastPage:false,
-    initialize: function() {
+    lastPage: false,
+    initialize: function () {
         EkstepRendererAPI.addEventListener('content:load:application/vnd.ekstep.epub-archive', this.launch, this);
         EkstepRendererAPI.addEventListener('renderer:content:replay', this.resetContent, this);
         EkstepRendererAPI.dispatchEvent('renderer:overlay:show');
         this.initContent();
     },
-    initContent: function(event, data) {
+    initContent: function (event, data) {
         var instance = this;
         data = content;
 
         var epubURL = isbrowserpreview ? this.getAssetURL(data) : data.baseDir;
         var epubPath = epubURL + '/index.epub';
 
-        org.ekstep.pluginframework.resourceManager.loadResource(epubPath, 'TEXT', function(err, data) {
+        org.ekstep.pluginframework.resourceManager.loadResource(epubPath, 'TEXT', function (err, data) {
             if (err) {
-                showToaster("error", "Unable to open eBook!", { timeOut: 200000 })
+                showToaster("error", "Unable to open eBook!", {timeOut: 200000})
             } else {
                 EkstepRendererAPI.dispatchEvent("renderer:splash:hide");
                 instance.renderEpub(epubPath);
             }
         });
     },
-    renderEpub: function(epubPath) {
+    renderEpub: function (epubPath) {
         jQuery('#gameCanvas').remove();
-        jQuery('#gameArea').css({ left: '10%', top: '0px', width: "80%", height: "90%", margin: "5% 0 5% 0" });
+        jQuery('#gameArea').css({left: '10%', top: '0px', width: "80%", height: "90%", margin: "5% 0 5% 0"});
         var epubOptions = {
             width: document.getElementById('gameArea').offsetWidth,
             height: document.getElementById('gameArea').offsetHeight,
@@ -45,15 +44,14 @@ org.ekstep.contentrenderer.baseLauncher.extend({
         this.book = ePub(epubPath, epubOptions);
         this.book.forceSingle(true);
         this.book.renderTo('gameArea');
-        EkstepRendererAPI.dispatchEvent('renderer:overlay:show');
         jQuery('.reload-stage').hide();
         this.addEventHandlers();
         this.initProgressElements();
     },
-    addEventHandlers: function() {
+    addEventHandlers: function () {
         var instance = this;
-        EventBus.addEventListener('nextClick', function() {
-            if(instance.lastPage){
+        EventBus.addEventListener('nextClick', function () {
+            if (instance.lastPage) {
                 EkstepRendererAPI.dispatchEvent('renderer:content:end');
                 instance.removeProgressElements();
             }
@@ -61,62 +59,64 @@ org.ekstep.contentrenderer.baseLauncher.extend({
 
         });
 
-        EventBus.addEventListener('previousClick', function() {
-            instance.book.prevPage();
-            instance.lastPage=false;
+        EventBus.addEventListener('previousClick', function () {
+            if(instance.currentPage === 2) {
+                // This is needed because some ePubs do not go back to the cover page on `book.prevPage()`
+                instance.gotoStart();
+            } else {
+                instance.book.prevPage();
+            }
+            instance.lastPage = false;
         });
 
-        EventBus.addEventListener('actionReload', function() {
-            instance.resetContent();
+        instance.book.getToc().then(function (toc) {
+            instance.start = toc[0].cfi;
         });
 
-        instance.book.getToc().then(function(toc) {
-            instance.start = toc[0].href;
-        });
-
-        instance.book.generatePagination().then(function(data) {
+        instance.book.generatePagination().then(function (data) {
             instance.totalPages = data.length;
             instance.updateProgressElements();
         });
 
-        instance.book.on('book:pageChanged', function(data) {
-            instance.logTelemetryInteract({ currentPage: instance.currentPage });
-            instance.logTelemetryNavigate({ fromPage: instance.currentPage, toPage: data.anchorPage });
-            instance.currentPage = data.anchorPage;
-            instance.progress = data.percentage;
+        instance.book.on('book:pageChanged', function (data) {
+            instance.logTelemetryInteract({currentPage: instance.currentPage.toString()});
+            instance.logTelemetryNavigate({fromPage: instance.currentPage.toString(), toPage: data.anchorPage.toString()});
+            instance.currentPage = data.pageRange[1];
             instance.updateProgressElements();
-             if(instance.book.pagination.lastPage==data.anchorPage||instance.book.pagination.lastPage==data.pageRange[1]){
-                instance.lastPage=true;
-             }
+            if (instance.book.pagination.lastPage === data.anchorPage || instance.book.pagination.lastPage === data.pageRange[1]) {
+                instance.lastPage = true;
+            }
         });
     },
-    getAssetURL: function(content) {
+    getAssetURL: function (content) {
         var content_type = "epub/";
         var globalConfig = EkstepRendererAPI.getGlobalConfig();
         var path = window.location.origin + globalConfig.s3ContentHost + content_type;
-        path += content.status == "Live" ? content.identifier + "-latest" : content.identifier + "-snapshot";
+        path += content.status === "Live" ? content.identifier + "-latest" : content.identifier + "-snapshot";
         return path;
     },
-    resetContent: function() {
+    resetContent: function () {
         this.relaunch();
         this.gotoStart();
     },
-    gotoStart: function() {
+    gotoStart: function () {
         if (this.start) {
-            this.book.gotoCfi(this.book.pageList[0].cfi);
+            this.book.gotoCfi(this.start);
+            instance.currentPage = 1;
+            this.lastPage = false;
+            EkstepRendererAPI.dispatchEvent('renderer:overlay:hide');
+            EkstepRendererAPI.dispatchEvent('renderer:overlay:show');
+            this.removeProgressElements();
+            this.initProgressElements();
         }
-        this.lastPage = false;
-        EkstepRendererAPI.dispatchEvent('renderer:overlay:hide');
-        EkstepRendererAPI.dispatchEvent('renderer:overlay:show');
-        this.initProgressElements();
     },
-    logTelemetryInteract: function(data) {
+    logTelemetryInteract: function (data) {
         var oeInteractData = {
             type: "TOUCH",
             id: "",
             extype: "",
             eks: {
-                stageId: data.currentPage,
+                stageId: data.currentPage.toString(),
                 type: "TOUCH",
                 subtype: "",
                 extype: "",
@@ -129,30 +129,55 @@ org.ekstep.contentrenderer.baseLauncher.extend({
         };
         TelemetryService.interact(oeInteractData.type, oeInteractData.id, oeInteractData.extype, oeInteractData.eks);
     },
-    logTelemetryNavigate: function(data) {
-        TelemetryService.navigate(data.fromPage, data.toPage);
+    logTelemetryNavigate: function (data) {
+        TelemetryService.navigate(data.fromPage.toString(), data.toPage.toString());
     },
-    initProgressElements: function() {
+    initProgressElements: function () {
         // Add page number display container
-        var $pageDiv = jQuery('<div>', { id: 'page' }).css({ position: 'absolute', top: '5px', width: '40%', height: '30px', overflow: 'hidden', margin: '0 auto', left: 0, right: 0, 'text-align': 'center' });
+        var $pageDiv = jQuery('<div>', {id: 'page'}).css({
+            position: 'absolute',
+            top: '5px',
+            width: '40%',
+            height: '30px',
+            overflow: 'hidden',
+            margin: '0 auto',
+            left: 0,
+            right: 0,
+            'text-align': 'center'
+        });
         jQuery('#gameArea').parent().append($pageDiv);
 
         // Add progress bar
-        var $progressDiv = jQuery('<div>', { id: 'progress-container' }).css({ width: '80%', margin: '0 auto', position: 'absolute', top: 0, left: 0, right: 0 });
-        var $progressContainer = jQuery('<div>', { id: 'progress' }).css({ overflow: 'hidden', height: '0.33em', 'background-color': '#e5e5e5' });
-        var $progressBar = jQuery('<div>', { id: 'bar' }).css({ width: '0%', height: '0.33em', 'background-color': '#7f7f7f' });
+        var $progressDiv = jQuery('<div>', {id: 'progress-container'}).css({
+            width: '80%',
+            margin: '0 auto',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0
+        });
+        var $progressContainer = jQuery('<div>', {id: 'progress'}).css({
+            overflow: 'hidden',
+            height: '0.33em',
+            'background-color': '#e5e5e5'
+        });
+        var $progressBar = jQuery('<div>', {id: 'bar'}).css({
+            width: '0%',
+            height: '0.33em',
+            'background-color': '#7f7f7f'
+        });
         $progressContainer.append($progressBar);
         $progressDiv.append($progressContainer);
         jQuery('#gameArea').parent().append($progressDiv);
         this.updateProgressElements();
     },
-    removeProgressElements: function() {
+    removeProgressElements: function () {
         jQuery('#page').remove();
         jQuery('#progress-container').remove();
     },
-    updateProgressElements: function() {
+    updateProgressElements: function () {
         jQuery('#page').html(this.currentPage + ' of ' + this.totalPages);
-        jQuery('#bar').css({ width: (this.progress * 100) + '%' });
+        jQuery('#bar').css({width: ((this.currentPage / this.totalPages) * 100) + '%'});
     }
 });
 //# sourceURL=ePubRendererPlugin.js
