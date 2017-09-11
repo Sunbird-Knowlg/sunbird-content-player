0528e7bb74295fcf53724c75cc38dd552230bea9
manjunath davanam <manjunathd@ilimi.in>
2017-09-11 15:05:43 +0530

    Issue #1028 fix:Video mute and unmute issue fix on click of replay icon

--- js-libs/renderer/plugin/VideoPlugin.js
+++ js-libs/renderer/plugin/VideoPlugin.js
@@ -58,10 +58,10 @@ var VideoPlugin = Plugin.extend({
 
     initPlugin: function(data) {
         this._data = data;
+        this._data.muted = AudioManager.muted ? true : false; 
         if (this._data) {
             if (_.isUndefined(this._data.autoplay)) this._data.autoplay = true;
             if (_.isUndefined(this._data.controls)) this._data.controls = false;
-            if (_.isUndefined(this._data.muted)) this._data.muted = false;
         }
 
         _instance = this;
