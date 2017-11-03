e42d4a1bd86178bcbefde155b8e905d60c1cd521
Vivek kasture <vivek_kasture@techjoomla.com>
2017-11-03 14:36:12 +0530

    Issue #1338 fix: Corrected typos

--- player/public/js/iEvaluator.js
+++ player/public/js/iEvaluator.js
@@ -1,6 +1,6 @@
 /***
  * The iEvaluator plugin class is the interfce class, other custom evaluator should have to
- * extend this class with the given fefault methods.
+ * extend this class with the given default methods.
  * @class EkstepRenderer.Plugin
  * @author Vivek Kasture <vivek_kasture@techjoomla.com>
  */
@@ -10,10 +10,10 @@ IEvaluator = Class.extend({
         this.initPlugin();
     },
     evaluate: function(item){
-        console.error("Suclass is not implemented this method");
+        console.error("Subclass is not implemented this method");
     },
     reset: function(item) {
-        console.error("Suclass is not implemented this method");
+        console.error("Subclass is not implemented this method");
     },
     initPlugin: function(data) {
         this.evaluate();
