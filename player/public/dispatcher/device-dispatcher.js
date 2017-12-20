9f7e724044fd1878abd28d0cb0965f0b2bb8587a
akashgupta9990 <akash.gupta@tarento.com>
2017-12-20 16:01:35 +0530

    Issue #1538 fix: Calling same function for feedback telemetry in device as other telemetry

--- player/public/dispatcher/device-dispatcher.js
+++ player/public/dispatcher/device-dispatcher.js
@@ -3,10 +3,6 @@ org.ekstep.contentrenderer.deviceDispatcher = new(org.ekstep.contentrenderer.IDi
     initDispatcher: function() {},
     dispatch: function(event) {        
         var eventStr = (typeof event === "string") ? event : JSON.stringify(event);
-        if(event.eid.toUpperCase() === "FEEDBACK")
-            telemetry.send(eventStr, "sendFeedback");
-        else
-            telemetry.send(eventStr, "sendTelemetry");    
-        
+        telemetry.send(eventStr, "sendTelemetry");
     }
 }));
