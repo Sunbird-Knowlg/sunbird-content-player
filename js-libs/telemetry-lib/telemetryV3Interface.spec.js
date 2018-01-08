575ebfbd565ccfb71b4dcf859a7414a06ff79a1c
Manjunath Davanam <manjunathd@ilimi.in>
2018-01-08 14:27:33 +0530

    Issue #1574 feat: V3 lib test-case improvement

--- js-libs/telemetry-lib/telemetryV3Interface.spec.js
+++ js-libs/telemetry-lib/telemetryV3Interface.spec.js
@@ -16,20 +16,11 @@ describe("Telemetry tests", function() {
     });
 
     describe("Telemetry START", function() {
-        it("It should not call init if config spec is invalid", function() {
-            var newConfig = JSON.parse(JSON.stringify(config));
-            delete newConfig.uid;
-            data = JSON.parse('{ "type": "player" }');
-            spyOn(telemetryObj, "start").and.callThrough();
-            var telemetryElement = telemetryObj.start(newConfig, "abc", "123", data);
-            expect(telemetryElement).toBeUndefined();
-            expect(EkTelemetry.initialized).not.toBeTruthy();
-            expect(telemetryObj.start).toHaveBeenCalled();
-        })
         it("It should not call init if pdata spec is invalid in config", function() {
             var newConfig = JSON.parse(JSON.stringify(config));
             delete newConfig.pdata.id;
             data = JSON.parse('{ "type": "player" }');
+            data.uaspec = {}
             spyOn(telemetryObj, "start").and.callThrough();
             var telemetryElement = telemetryObj.start(newConfig, "abc", "123", data);
             expect(telemetryElement).toBeUndefined();
@@ -38,13 +29,10 @@ describe("Telemetry tests", function() {
         })
         it("It should not call init if object spec is invalid in config", function() {
             var newConfig = JSON.parse(JSON.stringify(config));
-            delete newConfig.object.id;
             data = JSON.parse('{ "type": "player" }');
+            data.object  = {}
             spyOn(telemetryObj, "start").and.callThrough();
-            var telemetryElement = telemetryObj.start(newConfig, "abc", "123", data);
-            expect(telemetryElement).toBeUndefined();
             expect(EkTelemetry.initialized).not.toBeTruthy();
-            expect(telemetryObj.start).toHaveBeenCalled();
         })
         it("It should not call init if required data is unavailable", function() {
             data = {};
@@ -55,24 +43,26 @@ describe("Telemetry tests", function() {
             expect(telemetryObj.start).toHaveBeenCalled();
 
         })
-        it("It should invoke init", function() {
-            data = JSON.parse('{ "type": "player" }');
+        it("It should not call init if required dspec is unavailable", function() {
+            data = {};
+            data.type = 'player';
+            data.dspec = {}
             spyOn(telemetryObj, "start").and.callThrough();
             var telemetryElement = telemetryObj.start(config, "abc", "123", data);
+            expect(telemetryElement).toBeUndefined();
+            expect(EkTelemetry.initialized).not.toBeTruthy();
+            expect(telemetryObj.start).toHaveBeenCalled();
+
+        })
+        it("It should invoke init", function() {
+            data = JSON.parse('{ "type": "player" }');
+            var context = {};
+            spyOn(telemetryObj, "start").and.callThrough();
+            var telemetryElement = telemetryObj.start(config, "abc", "123", data, context);
             expect(telemetryElement).not.toBeUndefined();;
             expect(EkTelemetry.initialized).toBeTruthy();
             expect(telemetryObj.start).toHaveBeenCalled();
         })
-        it("It should not call init if telemetry is already running", function() {
-            data = JSON.parse('{ "type": "player" }');
-            spyOn(telemetryObj, "start").and.callThrough();
-            var telemetryElement = telemetryObj.start(config, "abc", "123", data);
-            expect(telemetryElement).toBeUndefined();
-            expect(EkTelemetry.initialized).toBeTruthy();
-            expect(telemetryObj.start).toHaveBeenCalled();
-
-        })
-
     });
 
     describe("Telemetry IMPRESSION", function() {
@@ -80,31 +70,35 @@ describe("Telemetry tests", function() {
             data = JSON.parse('{"type": "view","pageid": "567","uri": "/content/preview/do_23833","visits": {"objid":"123","objtype":"story"}}')
             spyOn(telemetryObj, "impression").and.callThrough();
             // spyOn(instance, "testFunction").and.callThrough();
-            telemetryObj.impression(data.pageid, data.type, undefined, data.uri, data.visits);
+            var context = {};
+            telemetryObj.impression(data, context);
             expect(telemetryObj.impression).toHaveBeenCalled();
             // expect(instance.testFunction).toHaveBeenCalled();
         });
         it("It should return if the required spec is unavailable", function() {
             data = {};
             spyOn(telemetryObj, "impression").and.callThrough();
-            var telemetryImpression = telemetryObj.impression(data.pageid, data.type, undefined, data.uri, data.visits);
+            var telemetryImpression = telemetryObj.impression({},{});
             expect(telemetryObj.impression).toHaveBeenCalled();
             expect(telemetryImpression).toBeUndefined();
         });
         it("It should return if visit spec is invalid", function() {
-            data = JSON.parse('{"type": "view","pageid": "567","uri": "/content/preview/do_23833","visits": {"objid":"123"}}');
+            data = JSON.parse('{"type": "view","pageid": "567","uri": "/content/preview/do_23833","visits": {}}');
             spyOn(telemetryObj, "impression").and.callThrough();
-            var telemetryImpression = telemetryObj.impression(data.pageid, data.type, undefined, data.uri, data.visits);
+            var context = {};
+            var telemetryImpression = telemetryObj.impression(data, context);
             expect(telemetryObj.impression).toHaveBeenCalled();
             expect(telemetryImpression).toBeUndefined();
         });
+
     });
 
     describe("Telemetry INTERACT", function() {
         it("It should invoke interact", function() {
             data = JSON.parse('{"type": "LISTEN","id": "123","target": {"id": "targetId","ver": "1.0","type": "Plugin"},"spec": {"id": "targetId","ver": "1.0"}}')
             spyOn(telemetryObj, "interact").and.callThrough();
-            telemetryObj.interact(data);
+            var context = {};
+            telemetryObj.interact(data, context);
             expect(telemetryObj.interact).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -123,6 +117,7 @@ describe("Telemetry tests", function() {
         });
         it("It should return if plugin spec is invalid", function() {
             data = JSON.parse('{"type": "LISTEN","id": "123","spec": {"id": "targetId"}}');
+            data.plugin = {}
             spyOn(telemetryObj, "interact").and.callThrough();
             var telemetryInteract = telemetryObj.interact(data);
             expect(telemetryObj.interact).toHaveBeenCalled();
@@ -134,7 +129,8 @@ describe("Telemetry tests", function() {
         it("It should invoke assess", function() {
             data = JSON.parse('{"item": {"id":"123","maxscore":"1","exlength":"1.23","desc":"description","title":"title"},"pass": "true","score": "100","resvalues": [{"lhs":"9"}],"duration":"121212121"}');
             spyOn(telemetryObj, "assess").and.callThrough();
-            telemetryObj.assess(data);
+            var context = {};
+            telemetryObj.assess(data, context);
             expect(telemetryObj.assess).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -157,7 +153,8 @@ describe("Telemetry tests", function() {
         it("It should log telemetry response event", function() {
             data = JSON.parse('{"target": {"id": "targetId","ver": "1.0","type": "Plugin"},"type": "MATCH","values": [{"lhs":"option1"}]}');
             spyOn(telemetryObj, "response").and.callThrough();
-            telemetryObj.response(data);
+            var context = {}
+            telemetryObj.response(data, context);
             expect(telemetryObj.response).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -180,7 +177,8 @@ describe("Telemetry tests", function() {
         it("It should log telemetry interrupt event", function() {
             data = JSON.parse('{"type": "background","pageid": "1234"}')
             spyOn(telemetryObj, "interrupt").and.callThrough();
-            telemetryObj.interrupt(data);
+            var context = {}
+            telemetryObj.interrupt(data, context);
             expect(telemetryObj.interrupt).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -194,18 +192,20 @@ describe("Telemetry tests", function() {
 
     describe("Telemetry FEEDBACK", function() {
         it("It should log telemetry feedback event", function() {
+            var context = {}
             data = JSON.parse('{"rating": "2","comments": "Nice app"}')
             spyOn(telemetryObj, "feedback").and.callThrough();
-            telemetryObj.feedback(data);
+            telemetryObj.feedback(data, context);
             expect(telemetryObj.feedback).toHaveBeenCalled();
         });
     });
 
     describe("Telemetry SHARE", function() {
         it("It should log telemetry share event", function() {
+            var context = {}
             data = JSON.parse('{"items": [{"obj": {"id": "123","type": "Plugin","ver": "1.0"}}]}');
             spyOn(telemetryObj, "share").and.callThrough();
-            telemetryObj.share(data);
+            telemetryObj.share(data, context);
             expect(telemetryObj.share).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -237,7 +237,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry error event", function() {
             data = JSON.parse('{"err": "500","errtype": "MOBILEAPP","stacktrace": "xy","pageid": "101","object": {"id": "_11","type": "PLUGIN","ver": "1.0"},"plugin": {"id": "_11","ver": "1.0"}}');
             spyOn(telemetryObj, "error").and.callThrough();
-            telemetryObj.error(data);
+            telemetryObj.error(data, {});
             expect(telemetryObj.error).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -267,7 +267,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry log event", function() {
             data = JSON.parse('{"type": "app_update","level": "ERROR","message": "Error occurred in updating app","pageid": "101"}');
             spyOn(telemetryObj, "log").and.callThrough();
-            telemetryObj.log(data);
+            telemetryObj.log(data,{});
             expect(telemetryObj.log).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -283,7 +283,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry heartbeat event", function() {
             data = JSON.parse('{"type": "app_update","level": "ERROR","message": "Error occurred in updating app","pageid": "101"}');
             spyOn(telemetryObj, "heartbeat").and.callThrough();
-            telemetryObj.heartbeat(data);
+            telemetryObj.heartbeat(data, {});
             expect(telemetryObj.heartbeat).toHaveBeenCalled();
         });
     });
@@ -292,7 +292,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry search event", function() {
             data = JSON.parse('{"query": "app_update","size": "333","topn": [{"h1":"H!"}]}');
             spyOn(telemetryObj, "search").and.callThrough();
-            telemetryObj.search(data);
+            telemetryObj.search(data, {});
             expect(telemetryObj.search).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -308,7 +308,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry metrics event", function() {
             data = JSON.parse('{"metric1": "app_update","metric2": "333"}');
             spyOn(telemetryObj, "metrics").and.callThrough();
-            telemetryObj.metrics(data);
+            telemetryObj.metrics(data, {});
             expect(telemetryObj.metrics).toHaveBeenCalled();
         });
     });
@@ -317,7 +317,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry metrics event", function() {
             data = JSON.parse('{"type": "PLUGIN","starttime": "2897238927","endtime": "98278233328","timespent":"95380994401","pageviews": "5","interactions": "4"}');
             spyOn(telemetryObj, "summary").and.callThrough();
-            telemetryObj.summary(data);
+            telemetryObj.summary(data, {});
             expect(telemetryObj.summary).toHaveBeenCalled();
         });
         it("It should return if the required data is unavailable", function() {
@@ -334,7 +334,7 @@ describe("Telemetry tests", function() {
         it("It should log telemetry exdata event", function() {
             var data = JSON.parse('{"type": "background","data": ""}');
             spyOn(telemetryObj, "exdata").and.callThrough();
-            telemetryObj.exdata(data.type, data.data);
+            telemetryObj.exdata(data, {});
             expect(telemetryObj.exdata).toHaveBeenCalled();
         });
     });
@@ -351,16 +351,100 @@ describe("Telemetry tests", function() {
         it("It should log telemetry END event", function() {
             data = JSON.parse('{"type": "ECML","pageid": "123"}');
             spyOn(telemetryObj, "end").and.callThrough();
-            telemetryObj.end(data);
+            telemetryObj.end(data, {});
             expect(telemetryObj.end).toHaveBeenCalled();
-            expect(telemetry.initialized).toBeFalsy();
         })
         it("It should not log telemetry END event if start is not happened", function() {
             data = JSON.parse('{"type": "ECML","pageid": "123"}');
             spyOn(telemetryObj, "end").and.callThrough();
             telemetryObj.end(data);
             expect(telemetryObj.end).toHaveBeenCalled();
-            expect(telemetry.initialized).toBeFalsy();
         })
     });
+
+    describe('Util methods',function(){
+        it('It should return the status of initialization',function(){
+             data = JSON.parse('{ "type": "player" }');
+            var context = {};
+            spyOn(telemetryObj, "start").and.callThrough();
+            var telemetryElement = telemetryObj.start(config, "abc", "123", data, context);
+            expect(telemetryObj.start).toHaveBeenCalled();
+            spyOn(telemetryObj,'isInitialized').and.callThrough();
+            var isInitialized = telemetryObj.isInitialized();
+            expect(isInitialized).toBeTruthy();
+        });
+
+        it('It should reset the context',function(){
+            spyOn(telemetryObj,'resetContext').and.callThrough();
+            telemetryObj.resetContext({channel:'in.ilimi'});
+            expect(telemetryObj.resetContext).toHaveBeenCalled();
+            var currentContext = telemetryObj.getCurrentContext();
+            expect(currentContext).not.toBeUndefined();
+            console.log("currentContext",currentContext)
+            expect(currentContext.channel).toEqual('in.ilimi')
+        });
+
+        it('It should reset the context',function(){
+            spyOn(telemetryObj,'resetContext').and.callThrough();
+            telemetryObj.resetContext({channel:'in.ilimi'});
+            expect(telemetryObj.resetContext).toHaveBeenCalled();
+            var currentContext = telemetryObj.getCurrentContext();
+            expect(currentContext).not.toBeUndefined();
+            console.log("currentContext",currentContext)
+            expect(currentContext.channel).toEqual('in.ilimi')
+        });
+        it("Should validate the has required data", function() {
+            spyOn(telemetryObj, "summary").and.callThrough();
+            telemetryObj.summary(undefined, {});
+            expect(telemetryObj.summary).toHaveBeenCalled();
+        });
+    })
+
+    describe('Invoke initialization',function(){
+        it('It should not invoke initialize, when invalid pdata',function(){
+            EkTelemetry.initialized = false;
+            var config = {};
+            config.pdata = {};
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(config);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+        it('It should not invoke initialize, when invalid object',function(){
+            EkTelemetry.initialized = false;
+            var config = {};
+            config.object = {};
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(config);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+        it('It should invoke initialize, when invalid object',function(){
+            EkTelemetry.initialized = false;
+            var config = {};
+            config.object = {};
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(config);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+        it('It should not invoke initialize, when requiredData is not passed',function(){
+            EkTelemetry.initialized = false;
+            var config = {};
+            config.object = {type:'player',id:"3758-6436"};
+            config.pdata = {id:"do_3432423"}
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(config);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+        it('It should not initialize the telemetry if it is already initialized',function(){
+            EkTelemetry.initialized = true;
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(config);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+        it('It should set config to empty,',function(){
+            EkTelemetry.initialized = false;
+            spyOn(telemetryObj, 'initialize').and.callThrough();
+            telemetryObj.initialize(undefined);
+            expect(telemetryObj.initialize).toHaveBeenCalled();
+        })
+    })
 });
