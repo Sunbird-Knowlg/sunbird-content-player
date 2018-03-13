e301f3772f63448b8b1f6c5d53cc80037105adee
akashgupta9990 <akash.gupta@tarento.com>
2018-03-13 18:27:27 +0530

    Issue #1601 fix: Made splash screen data as configurable

--- player/public/js/splashScreen.js
+++ player/public/js/splashScreen.js
@@ -1,18 +1,14 @@
 var splashScreen = {
     elementId: '#loading',
-    config: {
-        text: "Powered by EkStep Genie",
-        icon: "img/icons/icn_genie.png",
-        bgImage: "img/icons/background_1.png",
-        webLink: "https://www.ekstep.in"
-    },
     progressEle:undefined,
+    config: {},
     initialize: function() {
-        var appConfigKeys = Object.keys(AppConfig.splash);
+        var globalConfig = EkstepRendererAPI.getGlobalConfig();
+        var appConfigKeys = Object.keys(globalConfig.splash);
 
         for (var i = 0; i < appConfigKeys.length; i++) {
             var objKey = appConfigKeys[i];
-            splashScreen.config[objKey] = GlobalContext.config[objKey] || AppConfig.splash[objKey];
+            splashScreen.config[objKey] = globalConfig.splash[objKey];
         };
         var html = this.createHtml();
         jQuery(this.elementId).html(html);
@@ -32,7 +28,7 @@ var splashScreen = {
         EkstepRendererAPI.addEventListener("renderer:content:start", splashScreen.hide);
     },
     createHtml: function() {
-        var html = '<img src=' + splashScreen.config.bgImage + ' class="gc-loader-img" /><P class="splashText" id="splashTextId"> Loading your content ... </p><div id="progressArea"><div id="progressBar"></div><p id="progressCount" class="font-lato gc-loader-prog"></p></div><a href="' + splashScreen.config.webLink + '" target="_blank"><div id="splashScreen" class="splashScreen"> <img src=' + splashScreen.config.icon + ' class="splash-icon " /> <span>' + splashScreen.config.text + '</span> </div></a>';
+        var html = '<img src="' + splashScreen.config.bgImage + '" class="gc-loader-img" onerror="this.style.display=\'none\'" /><P class="splashText" id="splashTextId"> Loading your content ... </p><div id="progressArea"><div id="progressBar"></div><p id="progressCount" class="font-lato gc-loader-prog"></p></div><a href="' + splashScreen.config.webLink + '" target="_blank"><div id="splashScreen" class="splashScreen"> <img src="' + splashScreen.config.icon + '" class="splash-icon " onerror="this.style.display=\'none\'" /> <span>' + splashScreen.config.text + '</span> </div></a>';
         return html;
     },
 
@@ -44,7 +40,6 @@ var splashScreen = {
     },
 
     loadContentDetails: function(eve, data) {
-        console.log("loadContentDetails data: ", data);
         $("#splashTextId").text(data.name);
     },
 
