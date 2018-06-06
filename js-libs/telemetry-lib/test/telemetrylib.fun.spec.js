e01a80bc0072d4352fb161e005e959b7b8996263
Manjunath Davanam <manjunathd@ilimi.in>
2018-06-06 17:34:44 +0530

    Issue #1186 fix:  spec update

--- js-libs/telemetry-lib/test/telemetrylib.fun.spec.js
+++ js-libs/telemetry-lib/test/telemetrylib.fun.spec.js
@@ -21,7 +21,7 @@ describe("Telemetry Validation", function() {
             cb = callback;
             try {
                 document.addEventListener('TelemetryEvent', telemetryEventSuccess);
-                EkTelemetry.start(configurations, contentId, version, startEventData);
+                EkTelemetry.start(configurations, contentId, version, eventData);
             } catch (e) {
                 expect(e).not.toBe(undefined);
             }
@@ -32,18 +32,9 @@ describe("Telemetry Validation", function() {
             configurations = { "uid": "anonymous", "channel": "in.ekstep", "pdata": { "id": "in.ekstep", "ver": "1.0", "pid": "" }, "env": "preview", "sid": "", "did": "", "cdata": [{ "type": "worksheet", "id": "do_736298262" }], "rollup": { "l1": "", "l2": "" }, "object": { "id": "do_9823y23", "type": "Conten", "ver": "", "rollup": { "l1": "", "l2": "" } }, "batchsize": 20, "host": "https://api.ekstep.in", "endpoint": "/data/v3/telemetry", "tags": [], "apislug": "/action" }
             contentId = 'do_212432352355435435';
             version = '1.0';
-            startEventData = { type: 'content', mode: "preview", pageid: "" };
+            eventData = { type: 'content', mode: "preview", pageid: "" };
             done();
         })
-        xit("When valid event is passed", function(done) {
-            try {
-                EkTelemetry.start(configurations, contentId, version, eventData);
-                done();
-            } catch (e) {
-                expect(e).toBe(undefined);
-                done();
-            }
-        });
         describe('Configurations', function() {
             beforeEach(function(done) {
                 EkTelemetry.initialized = false;
@@ -52,33 +43,66 @@ describe("Telemetry Validation", function() {
             });
 
             describe('Channel', function() {
-                it(" When `channel:undefined`, Expect `channel:in.ekstep` ", function(done) {
+                it(" When `channel: undefined`, Expect `channel:in.ekstep` ", function(done) {
                     configurations.channel = undefined;
-                    callStartEvent(function(eventData) {
-                        expect(eventData.context.channel).toBe('in.ekstep');
+                    callStartEvent(function(res) {
+                        expect(res.context.channel).toBe('in.ekstep');
                         done();
                     });
                 });
-                it(" When `channel:x`, Expect `channel:x` ", function(done) {
+                it(" When `channel: null `, Expect `channel: in.ekstep` ", function(done) {
+                    configurations.channel = null;
+                    callStartEvent(function(res) {
+                        expect(res.context.channel).toBe('in.ekstep');
+                        done();
+                    });
+                });
+                it(" When `channel: ' ' `, Expect `channel: in.ekstep` ", function(done) {
+                    configurations.channel = ' ';
+                    callStartEvent(function(res) {
+                        expect(res.context.channel).toBe('in.ekstep');
+                        done();
+                    });
+                });
+                it(" When `channel: x`, Expect `channel:x` ", function(done) {
                     configurations.channel = 'x';
-                    callStartEvent(function(eventData) {
-                        expect(eventData.context.channel).toBe('x');
+                    callStartEvent(function(res) {
+                        expect(res.context.channel).toBe('x');
                         done();
                     });
                 });
-                xit(" When `channel:'' `, Expect `channel:in.ekstep` ", function(done) {
-                    configurations.channel = '';
-                    try {
-                        document.addEventListener('TelemetryEvent', function(event) {
-                            expect(event.detail.context.channel).toBe('in.ekstep');
-                            done();
-                        });
-                        EkTelemetry.start(configurations, contentId, version, eventData);
-                    } catch (e) {
-                        expect(e).not.toBe(undefined);
-                    }
-                })
-            })
+            });
+            describe('Env', function() {
+                it(" When `env: null`, Expect `env: ContentPlayer` ", function(done) {
+                    configurations.env = null;
+                    callStartEvent(function(res) {
+                        expect(res.context.env).toBe('ContentPlayer');
+                        done();
+                    });
+                });
+                it(" When `env: undefined `, Expect `env: ContentPlayer` ", function(done) {
+                    configurations.env = undefined;
+                    callStartEvent(function(res) {
+                        expect(res.context.env).toBe('ContentPlayer');
+                        done();
+                    });
+                });
+                it(" When `env: ' ' `, Expect `env: ' ' ` ", function(done) {
+                    configurations.env = ' ';
+                    callStartEvent(function(res) {
+                        expect(res.context.env).toBe(' ');
+                        done();
+                    });
+                });
+                it(" When `env: preview`, Expect `env:preview` ", function(done) {
+                    configurations.env = 'preview';
+                    callStartEvent(function(res) {
+                        expect(res.context.env).toBe('preview');
+                        done();
+                    });
+                });
+            });
+
         })
     });
     describe("END Event", function() {
@@ -88,26 +112,26 @@ describe("Telemetry Validation", function() {
                 document.addEventListener('TelemetryEvent', telemetryEventSuccess);
                 EkTelemetry.end(eventData, {});
             } catch (e) {
-               	cb(e);
+                cb(e);
             }
         }
-
         beforeEach(function(done) {
-            eventData = {"type":"content","pageid":"splash","summary":[{"progress":50}]};
+            eventData = { "type": "content", "pageid": "splash", "summary": [{ "progress": 50 }] };
             done();
         })
         it(" When `valid`, Expect `no error` ", function(done) {
-            callEndEvent(function(response) {
-                expect(response).toBeDefined();
+            callEndEvent(function(res) {
+                expect(res).toBeDefined();
                 done();
             });
         });
         it(" When `invalid`, Expect `throw error` ", function(done) {
-        	eventData.type = undefined;
+            eventData.type = undefined;
             callEndEvent(function(error) {
                 expect(error).not.toBeDefined();
-                console.log("Error Message",error);
-                expect(error.split(' ')).toContain('Invalid');
+                //var errors = error.split(' ');
+                console.log("Errors", errors);
+                expect(error).toMatch(/'type'/);
                 done();
             });
         });
