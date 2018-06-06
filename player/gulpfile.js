d92840353ba8a3173229becd76268b420b83436a
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-06-06 17:51:37 +0530

    Issue #1186 fix: gulp task added to generate telemetry schema

--- player/gulpfile.js
+++ player/gulpfile.js
@@ -1,52 +1,20 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
+gulp.task('build-telemetry-schema', function(){
+  var fs = require('fs');
+  var specFile = '../js-libs/telemetry-lib/telemetry-spec.js';
+  var schemas = [];
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
+  if(fs.existsSync(specFile)) {
+      fs.unlinkSync(specFile);
   }
-  done();
+  var files = fs.readdirSync('../js-libs/telemetry-lib/schema/');
+  files.forEach(function(file) {
+      if(file != 'event.json' && file != 'spec.js') {
+          var data = fs.readFileSync('../js-libs/telemetry-lib/schema/' + file, 'utf8');
+          schemas.push(JSON.parse(data));
+      }
+  })
+
+  fs.writeFileSync(specFile, 'var telemetrySchema = ' + JSON.stringify(schemas));
 });
