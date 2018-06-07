e8c40b88d8b3d5a39c57d4e951a0a8e584d5cfe3
Manjunath Davanam <manjunathd@ilimi.in>
2018-06-07 14:52:30 +0530

    Issue #1186 fix:  Sync Manager update

--- js-libs/telemetry-lib/telemetrySyncManager.js
+++ js-libs/telemetry-lib/telemetrySyncManager.js
@@ -7,55 +7,59 @@
 var TelemetrySyncManager = {
 
     /**
-    * This is the telemetry data for the perticular stage.
-    * @member {object} _teleData
-    * @memberof TelemetryPlugin
-    */
+     * This is the telemetry data for the perticular stage.
+     * @member {object} _teleData
+     * @memberof TelemetryPlugin
+     */
     _teleData: [],
-    init: function(){
+    init: function() {
         var instance = this;
         document.addEventListener('TelemetryEvent', this.sendTelemetry);
     },
     sendTelemetry: function(event) {
-        var Telemetry = EkTelemetry || Telemetry;
         var telemetryEvent = event.detail;
         console.log("Telemetry Events ", telemetryEvent);
         var instance = TelemetrySyncManager;
-        instance._teleData.push(Object.assign({},telemetryEvent));
-        if((telemetryEvent.eid.toUpperCase() === "END") || (instance._teleData.length >= Telemetry.config.batchsize)) {
-            var telemetryData = instance._teleData.splice(0, Telemetry.config.batchsize);
-            var telemetryObj = {
-                "id": "ekstep.telemetry",
-                "ver": Telemetry._version,
-                "ets": (new Date()).getTime(),
-                "events": telemetryData
-            };
-            var headersParam = {};
-            if ('undefined' != typeof Telemetry.config.authtoken)
-                headersParam["Authorization"] = 'Bearer ' + Telemetry.config.authtoken;
-
-            var fullPath = Telemetry.config.host + Telemetry.config.apislug + Telemetry.config.endpoint;
-            headersParam['dataType'] = 'json';
-            headersParam["Content-Type"] = "application/json";
-            jQuery.ajax({
-                url: fullPath,
-                type: "POST",
-                headers: headersParam,
-                data: JSON.stringify(telemetryObj)
-            }).done(function(resp) {
-                console.log("Telemetry API success", resp);
-            }).fail(function(error, textStatus, errorThrown) {
-                instance.mergeEvents(telemetryData);
-                if (error.status == 403) {
-                    console.error("Authentication error: ", error);
-                } else {
-                    console.log("Error while Telemetry sync to server: ", error);
-                }
-            });
+        instance._teleData.push(Object.assign({}, telemetryEvent));
+        if ((telemetryEvent.eid.toUpperCase() === "END") || (instance._teleData.length >= Telemetry.config.batchsize)) {
+            TelemetrySyncManager.syncEvents();
         }
     },
     mergeEvents: function(events) {
         TelemetrySyncManager._teleData = TelemetrySyncManager._teleData.concat(events);
+    },
+    syncEvents: function() {
+        var Telemetry = EkTelemetry || Telemetry;
+        var instance = TelemetrySyncManager;
+        var telemetryData = instance._teleData.splice(0, Telemetry.config.batchsize);
+        var telemetryObj = {
+            "id": "ekstep.telemetry",
+            "ver": Telemetry._version,
+            "ets": (new Date()).getTime(),
+            "events": telemetryData
+        };
+        var headersParam = {};
+        if ('undefined' != typeof Telemetry.config.authtoken)
+            headersParam["Authorization"] = 'Bearer ' + Telemetry.config.authtoken;
+
+        var fullPath = Telemetry.config.host + Telemetry.config.apislug + Telemetry.config.endpoint;
+        headersParam['dataType'] = 'json';
+        headersParam["Content-Type"] = "application/json";
+        jQuery.ajax({
+            url: fullPath,
+            type: "POST",
+            headers: headersParam,
+            data: JSON.stringify(telemetryObj)
+        }).done(function(resp) {
+            console.log("Telemetry API success", resp);
+        }).fail(function(error, textStatus, errorThrown) {
+            instance.mergeEvents(telemetryData);
+            if (error.status == 403) {
+                console.error("Authentication error: ", error);
+            } else {
+                console.log("Error while Telemetry sync to server: ", error);
+            }
+        });
     }
 }
 if (typeof document != 'undefined') {
