8bef168ec65689ad407c1da673f3031b3cb0e853
Manjunath Davanam <manjunathd@ilimi.in>
2018-07-31 18:58:14 +0530

    Issue #SB-5062 fix: Added event dispatcher

--- player/public/dispatcher/device-dispatcher.js
+++ player/public/dispatcher/device-dispatcher.js
@@ -1,7 +1,8 @@
 org.ekstep.contentrenderer.deviceDispatcher = new(org.ekstep.contentrenderer.IDispatcher.extend({
     type: "deviceDispatcher",
     initDispatcher: function() {},
-    dispatch: function(event) {        
+    dispatch: function(event) {
+        EventBus.dispatch('telemetryEvent', event);
         var eventStr = (typeof event === "string") ? event : JSON.stringify(event);
         telemetry.send(eventStr, "sendTelemetry");
     }
