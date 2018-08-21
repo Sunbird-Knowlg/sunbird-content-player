3d6148add9dafb9965aa59a50428c35e45615310
Rajesh Gupta <rajesh.gupta@compassites.net>
2018-08-21 15:47:50 +0530

    Issue #1722 fix:Groups and user selection screen is displaying on click of replay button

--- player/public/coreplugins/org.sunbird.player.endpage-1.1/renderer/endpageApp.js
+++ player/public/coreplugins/org.sunbird.player.endpage-1.1/renderer/endpageApp.js
@@ -45,7 +45,11 @@ endPage.controller("endPageController", function($scope, $rootScope, $state,$ele
     };
    
     $scope.replayContent = function() {
-        EventBus.dispatch("event:openUserSwitchingModal", {'logGEEvent': $scope.pluginInstance._isAvailable});
+        if(isbrowserpreview) {
+            $scope.replayCallback();
+        }else {
+            EventBus.dispatch("event:openUserSwitchingModal", {'logGEEvent': $scope.pluginInstance._isAvailable});
+        }
     };
     $scope.replayCallback = function(){
         EkstepRendererAPI.hideEndPage();
