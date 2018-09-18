649b63136f5d3b5075da1dee6c9d470337a1b1f9
Manjunath Davanam <manjunathd@ilimi.in>
2018-09-18 18:40:26 +0530

    Issue #SB-7116 fix: eslint fixes for player service files

--- player/public/services/controllerservice.js
+++ player/public/services/controllerservice.js
@@ -2,21 +2,20 @@
  * @author Manjunath Davanam <manjunathd@ilimi.in>
  */
 org.ekstep.service.controller = new(org.ekstep.service.mainService.extend({
-	loadModules: undefined,
-	loadTemplates: undefined,
+    loadModules: undefined,
+    loadTemplates: undefined,
     callback: undefined,
-	init:function(){
-	},
-	initService: function(loadModuleFn) {
-        this.loadModules = loadModuleFn;
+    init: function() {},
+    initService: function(loadModuleFn) {
+        this.loadModules = loadModuleFn
     },
     loadNgModules: function(templatePath, controllerPath, callback) {
-        this.loadModules && this.loadModules(templatePath, controllerPath, callback);
+        this.loadModules && this.loadModules(templatePath, controllerPath, callback)
     },
-    injectTemplate:function(injectTemplatesFn){
-    	this.loadTemplates = injectTemplatesFn;
+    injectTemplate: function(injectTemplatesFn) {
+        this.loadTemplates = injectTemplatesFn
     },
-    inject:function(templatePath, toElement){
-    	this.loadTemplates && this.loadTemplates(templatePath, toElement);
+    inject: function(templatePath, toElement) {
+        this.loadTemplates && this.loadTemplates(templatePath, toElement)
     }
-}));
+}))()
