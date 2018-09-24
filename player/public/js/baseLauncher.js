dec020b916f552eb384135fb124ff90904bf41f0
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-09-24 22:07:22 +0530

    Issue #SB-7234 fix: Added sleepmode in telemetryend

--- player/public/js/baseLauncher.js
+++ player/public/js/baseLauncher.js
@@ -63,6 +63,7 @@ org.ekstep.contentrenderer.baseLauncher = Class.extend({
      * @memberof org.ekstep.contentrenderer.baseLauncher
      */
 	end: function () {
+		if (this.sleepMode) return
 		this.heartBeatEvent(false)
 		this.endTelemetry()
 		EkstepRendererAPI.dispatchEvent("renderer:endpage:show")
@@ -121,6 +122,7 @@ org.ekstep.contentrenderer.baseLauncher = Class.extend({
      * Generation of OE_END Telemetry event.
      */
 	endTelemetry: function (event) {
+		if (this.sleepMode) return
 		if (TelemetryService.instance && TelemetryService.instance.telemetryStartActive()) {
 			var telemetryEndData = {}
 			telemetryEndData.stageid = getCurrentStageId()
