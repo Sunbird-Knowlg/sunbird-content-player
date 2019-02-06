04d61b4d388f729f0476874833623c0bf926728d
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-02-06 10:49:35 +0530

    Issue #SB-000 fix: eslint issue fixes

--- player/gulpfile.js
+++ player/gulpfile.js
@@ -1,20 +1,20 @@
-var gulp = require('gulp');
+var gulp = require("gulp")
 
-gulp.task('build-telemetry-schema', function(){
-  var fs = require('fs');
-  var specFile = '../js-libs/telemetry-lib/telemetry-spec.js';
-  var schemas = [];
+gulp.task("build-telemetry-schema", function () {
+	var fs = require("fs")
+	var specFile = "../js-libs/telemetry-lib/telemetry-spec.js"
+	var schemas = []
 
-  if(fs.existsSync(specFile)) {
-      fs.unlinkSync(specFile);
-  }
-  var files = fs.readdirSync('../js-libs/telemetry-lib/schema/');
-  files.forEach(function(file) {
-      if(file != 'event.json' && file != 'spec.js') {
-          var data = fs.readFileSync('../js-libs/telemetry-lib/schema/' + file, 'utf8');
-          schemas.push(JSON.parse(data));
-      }
-  })
+	if (fs.existsSync(specFile)) {
+		fs.unlinkSync(specFile)
+	}
+	var files = fs.readdirSync("../js-libs/telemetry-lib/schema/")
+	files.forEach(function (file) {
+		if (file !== "event.json" && file !== "spec.js") {
+			var data = fs.readFileSync("../js-libs/telemetry-lib/schema/" + file, "utf8")
+			schemas.push(JSON.parse(data))
+		}
+	})
 
-  fs.writeFileSync(specFile, 'var telemetrySchema = ' + JSON.stringify(schemas));
-});
+	fs.writeFileSync(specFile, "var telemetrySchema = " + JSON.stringify(schemas))
+})
