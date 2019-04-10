280df10baea64e124d2f29e0dab38424d6487512
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-04-10 16:07:00 +0530

    Issue #SB-9994 feat: removed spaces

--- player/public/js/main.js
+++ player/public/js/main.js
@@ -235,23 +235,23 @@ function compareObject (obj1, obj2) {
 		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false
 
 		switch (typeof (obj1[p])) {
-			// Deep compare objects
-			case "object":
-				if (!Object.compare(obj1[p], obj2[p])) return false
-				break
+		// Deep compare objects
+		case "object":
+			if (!Object.compare(obj1[p], obj2[p])) return false
+			break
 			// Compare function code
-			case "function":
-				if (typeof (obj2[p]) === "undefined" || (p !== "compare" && obj1[p].toString() !== obj2[p].toString())) return false
-				break
+		case "function":
+			if (typeof (obj2[p]) === "undefined" || (p !== "compare" && obj1[p].toString() !== obj2[p].toString())) return false
+			break
 			// Compare values
-			default:
-				if (obj1[p] !== obj2[p]) return false
+		default:
+			if (obj1[p] !== obj2[p]) return false
 		}
 	}
 
 	// Check object 2 for any extra properties
 	// eslint-disable-next-line
-	for (var p in obj2) {
+for (var p in obj2) {
 		if (typeof (obj1[p]) === "undefined") return false
 	}
 	return true
