16a9a0b21bd5a44a5da26bfd980e3fd237e7d213
Jagadish Pujari <jagadish.pujari@tarento.com>
2019-05-09 15:34:28 +0530

    Issue #11432 fix: renamed assessEventVer to eventVer

--- js-libs/telemetry/TelemetryV3Manager.js
+++ js-libs/telemetry/TelemetryV3Manager.js
@@ -117,7 +117,7 @@ TelemetryV3Manager = Class.extend({
             var v3questionItem = {
                 id: eventObj.event.edata.eks.qid,
                 maxscore: eventObj.event.edata.eks.maxscore,
-                eventVer: data.assessEventVer,
+                eventVer: data.eventVer,
                 type: data.type,
                 exlength: 0,
                 params: data.params || eventObj.event.edata.eks.params || [],
