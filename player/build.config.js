0908919124b13ed0eb8a675d37467676695bcd5e
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-09-20 18:42:56 +0530

    Issue #SB-13987 fix: menu issue fixed

--- player/build.config.js
+++ player/build.config.js
@@ -9,11 +9,11 @@ module.exports = {
 			backgroundImage: "./public/assets/icons/background_1.png"
 		},
 		plugins: [{ id: "org.ekstep.overlay", ver: "1.0", minify: true, package: true },
+			{ id: "org.ekstep.nextnavigation", ver: "1.0", minify: true, package: true },
+			{ id: "org.ekstep.previousnavigation", ver: "1.0", minify: true, package: true },
 			{ id: "org.ekstep.userswitcher", ver: "1.0", minify: true, package: true },
 			{ id: "org.ekstep.ecmlrenderer", ver: "1.0", minify: true, package: false },
-			{ id: "org.ekstep.endpage", ver: "1.0", minify: false, package: true },
-			{ id: "org.ekstep.nextnavigation", ver: "1.0", minify: false, package: true },
-			{ id: "org.ekstep.previousnavigation", ver: "1.0", minify: false, package: true }
+			{ id: "org.ekstep.endpage", ver: "1.0", minify: false, package: true }
 		]
 	},
 	sunbird: {
@@ -23,13 +23,13 @@ module.exports = {
 		},
 		plugins: [
 			{ id: "org.ekstep.overlay", ver: "1.0", minify: true, package: true },
+			{ id: "org.ekstep.nextnavigation", ver: "1.0", minify: true, package: true },
+			{ id: "org.ekstep.previousnavigation", ver: "1.0", minify: true, package: true },
 			{ id: "org.sunbird.player.userswitcher", ver: "1.0", minify: true, package: true },
 			{ id: "org.ekstep.ecmlrenderer", ver: "1.0", minify: true, package: false },
 			{ id: "org.sunbird.player.endpage", ver: "1.1", minify: false, package: true },
 			{ id: "org.ekstep.videorenderer", ver: "1.1", minify: false, package: true },
-			{ id: "org.ekstep.pdfrenderer", ver: "1.0", minify: false, package: true },
-			{ id: "org.ekstep.nextnavigation", ver: "1.0", minify: false, package: true },
-			{ id: "org.ekstep.previousnavigation", ver: "1.0", minify: false, package: true }
+			{ id: "org.ekstep.pdfrenderer", ver: "1.0", minify: false, package: true }
 		]
 	}
 }
