c53824f7f4c494224e1d41845ff32c422922a4cb
Gourav More <gourav_m@tekditechnologies.com>
2019-09-26 19:47:31 +0530

    Issue #SB-14688 fix: [Offline mode] Unable to Play content in offline Mode

--- player/public/services/interfaceService.js
+++ player/public/services/interfaceService.js
@@ -117,7 +117,7 @@ org.ekstep.service.content = new (org.ekstep.service.mainService.extend({
 
 		if (item.path && data) {
 			var path = (item.path.charAt(item.path.length - 1) === "/") ? item.path.substring(0, item.path.length - 1) : item.path
-			path = (window.cordova) ? "file://" + path : path
+			//path = (window.cordova) ? "file://" + path : path
 			data.baseDir = path
 			globalConfig.basepath = path
 			if (typeof cordova !== "undefined") { data.appIcon = (data.appIcon) ? path + "/" + data.appIcon : path + "/logo.png" } else { data.appIcon = (isbrowserpreview) ? data.appIcon : path + "/logo.png" }
