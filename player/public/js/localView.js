77193a39b5e0b804571cfb31f78f07c4d2887952
Gourav More <gourav_m@tekditechnologies.com>
2019-11-27 12:53:59 +0530

    Issue #SB-15647 fix: offline desktop app issue

--- player/public/js/localView.js
+++ player/public/js/localView.js
@@ -3,8 +3,6 @@ org.ekstep.view.local = new (org.ekstep.view.mainView.extend({
         console.log("======Local View======");
         this.setGlobalContext().then(function (appInfo) {
             if (typeof localPreview !== "undefined" && localPreview === "local") { return }
-
-            // commented for now for offline desktop app. if we remove this, player will not work in local
             org.ekstep.contentrenderer.startGame(GlobalContext.config.appInfo)
         }).catch(function (res) {
             console.log("Error Globalcontext.init:", res)
