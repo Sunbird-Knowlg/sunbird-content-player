57b6cdbcf04e31bb9facad2a4c4857f8311e6bcf
Gourav More <gourav_m@tekditechnologies.com>
2019-12-17 19:38:37 +0530

    Issue #SB-16591 fix: Video fails to appear on mobile after scanning a course completion certificate QR code

--- player/public/js/ekstepRendererApi.js
+++ player/public/js/ekstepRendererApi.js
@@ -7,6 +7,9 @@
  * @author Vinu Kumar <vinu.kumat@tarento.com>
  */
 window.EkstepRendererAPI = {
+	isMobile: function(){
+		return (/version\//i.test(navigator.userAgent.toLowerCase()))
+	},
 	baseURL: "",
 	/**
      * Register an event listener callback function for the events raised by the framework.
