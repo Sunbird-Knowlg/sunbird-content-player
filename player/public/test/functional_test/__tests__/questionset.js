c2ec827d1bf17ec84635c4cb95d9a8fb892b4bcd
“Rajnish <rajnish_d@techjoomla.com>
2020-03-16 19:49:29 +0530

    Functionality test of question set

--- player/public/test/functional_test/__tests__/questionset.js
+++ player/public/test/functional_test/__tests__/questionset.js
@@ -51,35 +51,29 @@ describe(
               });
             await page.close()
         })
-
         it('Player shell loaded with fixture stories', async () => {
             await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
             await page.screenshot({path: '__tests__/screenshots/fixture_stories.png'});
         })
-
         it('Should open question set', async() => {
             const playQuestion = await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
             await playQuestion.click()
             await page.screenshot({path: '__tests__/screenshots/onclick_questionset.png'});
         })
-
         it('Zoom button should open the image in zoomed view', async () => {
             const zoomImg = await page.waitForSelector('#org-ekstep-contentrenderer-questionunit-questionComponent-ZoomImg')
             await zoomImg.click()
             await page.screenshot({path: '__tests__/screenshots/onzoom_image.png'});
         })
-
         it('check pop exists or not', async()=>{
             const popup = await page.waitForSelector('#image-model-popup > div.popup-full-body > div > div')
             await popup.click()
             await page.screenshot({path: '__tests__/screenshots/afterpop_close.png'});
         })
-
         it('Test for title', async () => {
             const title = await page.waitForSelector('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p')
             await page.screenshot({path: '__tests__/screenshots/test_title.png'});
         })
-
         it('Question title font size should be same in Mobile and Web and height and width should be different', async() => {
             const btnStylesInWeb = await page.$eval('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p', btnStylesInWeb => JSON.parse(JSON.stringify(getComputedStyle(btnStylesInWeb))));
             await page.emulate(devices['iPhone 4'])
@@ -93,13 +87,11 @@ describe(
             expect(parseFloat(btnStylesInWeb.height)).not.toEqual(parseFloat(btnStylesInMobile.height)) 
             expect(parseFloat(btnStylesInWeb.width)).not.toEqual(parseFloat(btnStylesInMobile.width)) 
         })
-
         it('Test for Question Set', async () => {
             await page.setViewport({width: 1280,height: 800})
             const playQuestion = await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
             await playQuestion.click()
         })
-
         it('Test for mcq', async() => {
             await page.waitForSelector('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p')
             const wrongAnswer = await page.waitForSelector('#mcq-question-container > div.bg-graphics-2 > div.outer-option-container.horizontal > div > div > div:nth-child(2)')
@@ -115,14 +107,12 @@ describe(
             const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
             await nextPopupButton.click()
         })
-
         it('Skipping match test',async() => {
             const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click()
             const popupNextButton = await page.waitForSelector('#popup-buttons-container > div.left.button')
             await popupNextButton.click()
         })
-
         it('Input type test', async() => {
             await page.waitForSelector('#ans-field1');
             await page.evaluate(() => {
@@ -138,7 +128,6 @@ describe(
             const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
             await nextPopupButton.click();
         })
-
         it('Skipping arranging test', async() => {
             const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click()
@@ -174,8 +163,7 @@ describe(
             await nextButton.click()
             const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
             await nextPopupButton.click();
-        })
-           
+        }) 
         it('Rating and comment test', async() => {
             const ratingFour = await page.waitForSelector('#gcFbPopup > div.gc-popup-new.gc-fc-popup > div.gc-popup-body-new > div > div:nth-child(1) > div:nth-child(4) > img')
             await ratingFour.click()
@@ -191,8 +179,6 @@ describe(
             await submitButton.click()
             await page.screenshot({path: '__tests__/screenshots/submit_rating.png'});
         })
-
-
     },
     timeout
 )
