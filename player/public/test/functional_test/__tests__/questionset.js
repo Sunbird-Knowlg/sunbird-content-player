9e7f8055edfb71714ccc9ff92e3c26bc757d86ab
“Rajnish <rajnish_d@techjoomla.com>
2020-03-16 19:01:23 +0530

    Functionality test of question set

--- player/public/test/functional_test/__tests__/questionset.js
+++ player/public/test/functional_test/__tests__/questionset.js
@@ -54,25 +54,30 @@ describe(
 
         it('Player shell loaded with fixture stories', async () => {
             await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
+            await page.screenshot({path: '__tests__/screenshots/fixture_stories.png'});
         })
 
         it('Should open question set', async() => {
             const playQuestion = await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
             await playQuestion.click()
+            await page.screenshot({path: '__tests__/screenshots/onclick_questionset.png'});
         })
 
         it('Zoom button should open the image in zoomed view', async () => {
             const zoomImg = await page.waitForSelector('#org-ekstep-contentrenderer-questionunit-questionComponent-ZoomImg')
             await zoomImg.click()
+            await page.screenshot({path: '__tests__/screenshots/onzoom_image.png'});
         })
 
         it('check pop exists or not', async()=>{
             const popup = await page.waitForSelector('#image-model-popup > div.popup-full-body > div > div')
             await popup.click()
+            await page.screenshot({path: '__tests__/screenshots/afterpop_close.png'});
         })
 
         it('Test for title', async () => {
             const title = await page.waitForSelector('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p')
+            await page.screenshot({path: '__tests__/screenshots/test_title.png'});
         })
 
         it('Question title font size should be same in Mobile and Web and height and width should be different', async() => {
@@ -84,15 +89,18 @@ describe(
             await page.waitForSelector('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p')
             const btnStylesInMobile = await page.$eval('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p', btnStylesInMobile => JSON.parse(JSON.stringify(getComputedStyle(btnStylesInMobile))));
             // console.info("font size is" + btnStylesInWeb.fontsize)
-            expect(btnStylesInWeb.fontsize).toEqual(btnStylesInMobile.fontsize) 
-            expect(btnStylesInWeb.height).not.toEqual(btnStylesInMobile.height) 
-            expect(btnStylesInWeb.width).not.toEqual(btnStylesInMobile.width) 
+            expect(parseFloat(btnStylesInWeb.fontsize)).toEqual(parseFloat(btnStylesInMobile.fontsize)) 
+            expect(parseFloat(btnStylesInWeb.height)).not.toEqual(parseFloat(btnStylesInMobile.height)) 
+            expect(parseFloat(btnStylesInWeb.width)).not.toEqual(parseFloat(btnStylesInMobile.width)) 
         })
 
         it('Test for Question Set', async () => {
             await page.setViewport({width: 1280,height: 800})
             const playQuestion = await page.waitForSelector('body > div:nth-child(7) > div > ion-pane > ion-content > div > div:nth-child(38)')
             await playQuestion.click()
+        })
+
+        it('Test for mcq', async() => {
             await page.waitForSelector('#mcq-question-container > div.question-content-container > div > div.hiding-container > div > p')
             const wrongAnswer = await page.waitForSelector('#mcq-question-container > div.bg-graphics-2 > div.outer-option-container.horizontal > div > div > div:nth-child(2)')
             const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
@@ -106,9 +114,16 @@ describe(
             await nextButton.click()
             const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
             await nextPopupButton.click()
+        })
+
+        it('Skipping match test',async() => {
+            const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click()
             const popupNextButton = await page.waitForSelector('#popup-buttons-container > div.left.button')
             await popupNextButton.click()
+        })
+
+        it('Input type test', async() => {
             await page.waitForSelector('#ans-field1');
             await page.evaluate(() => {
                 document.querySelector('#ans-field1').value = 'jupiter';
@@ -117,45 +132,66 @@ describe(
             await page.evaluate(() => {
                 document.querySelector('#ans-field2').value = 'mercury';
             });
+            await page.screenshot({path: '__tests__/screenshots/setting_fields.png'});
+            const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click();
-            const nextPopupButton2 = await page.waitForSelector('#popup-buttons-container > div')
-            await nextPopupButton2.click();
+            const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
+            await nextPopupButton.click();
+        })
+
+        it('Skipping arranging test', async() => {
+            const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click()
-            const popupNextButton2 = await page.waitForSelector('#popup-buttons-container > div.left.button')
-            await popupNextButton2.click()
+            const popupNextButton = await page.waitForSelector('#popup-buttons-container > div.left.button')
+            await popupNextButton.click()
+        })
+        it('Sentence creation test', async() => {
+            const select0 = await page.waitForSelector('#w0')
+            await select0.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_what.png'});
 
-            const select1 = await page.waitForSelector('#w0')
+            const select1 = await page.waitForSelector('#w1')
             await select1.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_are.png'});
 
-            const select2 = await page.waitForSelector('#w1')
+            const select2 = await page.waitForSelector('#w2')
             await select2.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_you.png'});
 
             const select3 = await page.waitForSelector('#w3')
             await select3.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_looking.png'});
 
             const select4 = await page.waitForSelector('#w4')
             await select4.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_for.png'});
 
             const select5 = await page.waitForSelector('#w5')
             await select5.click()
+            await page.screenshot({path: '__tests__/screenshots/setting_questionmark.png'});
 
+            const nextButton = await page.waitForSelector('body > div:nth-child(8) > div > div > custom-next-navigation > div > a > img')
             await nextButton.click()
-            const nextPopupButton3 = await page.waitForSelector('#popup-buttons-container > div')
-            await nextPopupButton3.click();
-
+            const nextPopupButton = await page.waitForSelector('#popup-buttons-container > div')
+            await nextPopupButton.click();
+        })
+           
+        it('Rating and comment test', async() => {
             const ratingFour = await page.waitForSelector('#gcFbPopup > div.gc-popup-new.gc-fc-popup > div.gc-popup-body-new > div > div:nth-child(1) > div:nth-child(4) > img')
-            ratingFour.click()
+            await ratingFour.click()
+            await page.screenshot({path: '__tests__/screenshots/four_rating.png'});
 
             await page.waitForSelector('#commentText');
             await page.evaluate(() => {
             document.querySelector('#commentText').value = 'test comment';
             });
+            await page.screenshot({path: '__tests__/screenshots/comment_rating.png'});
 
             const submitButton = await page.waitForSelector('#gcFbPopup > div.gc-popup-new.gc-fc-popup > div.gc-popup-body-new > div')
             await submitButton.click()
-   
-            
-       })
+            await page.screenshot({path: '__tests__/screenshots/submit_rating.png'});
+        })
+
 
     },
     timeout
