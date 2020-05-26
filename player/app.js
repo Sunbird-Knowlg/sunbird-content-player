6a3a5cef985b0a859a05f9c72b2652777a8582ec
Rahul Shukla <rahul.shukla13@gmail.com>
2020-05-26 15:47:22 +0530

    Issue #SB-18100 chore: Remove security vulnerability from @project-sunbird/content-player

--- player/app.js
+++ player/app.js
@@ -14,12 +14,18 @@
  */
 appConfig = require("./app-data/appConfig.json")
 var express = require("express")
-
 var http = require("http")
-
 var path = require("path")
-
+// var favicon = require('serve-favicon')
+var morgan = require('morgan')
+var compression = require('compression')
+var methodOverride = require('method-override')
+var cookieParser = require('cookie-parser')
+var serveStatic = require('serve-static')
+var errorhandler = require('errorhandler')
+var bodyParser = require('body-parser')
 var fs = require("fs")
+
 http.globalAgent.maxSockets = 100000
 
 var app = express()
@@ -29,36 +35,52 @@ app.set("port", 3000)
 // eslint-disable-next-line
 app.set("views", __dirname + "/views")
 app.set("view engine", "ejs")
-app.use(express.favicon())
-app.use(express.logger("dev"))
-app.use(express.compress())
+//app.use(express.favicon())
+// app.use(favicon())
+// app.use(express.logger("dev"))
+app.use(morgan('dev'));
+// app.use(express.compress())
+app.use(compression())
 app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.bodyParser())
-app.use(express.methodOverride())
-app.use(express.cookieParser())
 
-app.use(app.router)
-app.locals({
-	contextPath: ""
-})
+// app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// app.use(express.bodyParser())
+app.use(bodyParser.json())
+// app.use(express.methodOverride())
+app.use(methodOverride())
+// app.use(express.cookieParser())
+app.use(cookieParser())
 
-app.use(express.compress())
-app.use(express.static(path.join(__dirname, "public")))
-app.use(express.static(path.join(__dirname, "../js-libs/")))
-app.use(express.static(path.join(__dirname, "../player/node_modules/")))
-app.use(express.static(path.join(__dirname, "views")))
+
+// app.use(app.router)
+app.locals = {
+    contextPath: ""
+}
+
+// app.use(express.compress())
+// app.use(express.static(path.join(__dirname, "public")))
+app.use(serveStatic(path.join(__dirname, "public")))
+// app.use(express.static(path.join(__dirname, "../js-libs/")))
+app.use(serveStatic(path.join(__dirname, "../js-libs/")))
+// app.use(express.static(path.join(__dirname, "../player/node_modules/")))
+app.use(serveStatic(path.join(__dirname, "../player/node_modules/")))
+// app.use(express.static(path.join(__dirname, "views")))
+app.use(serveStatic(path.join(__dirname, "views")))
 
 // development only
 if (app.get("env") === "development") {
-	app.use(express.errorHandler())
+    app.use(errorhandler())
 }
 
 // Bootstrap routes
 // eslint-disable-next-line
-var routes = __dirname + "/server/routes"; var route_files = fs.readdirSync(routes)
-route_files.forEach(function (file) {
-	require(routes + "/" + file)(app, __dirname)
+var routes = __dirname + "/server/routes";
+var route_files = fs.readdirSync(routes)
+route_files.forEach(function(file) {
+    require(routes + "/" + file)(app, __dirname)
 })
 
 // Workers can share any TCP connection
