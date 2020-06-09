b47417e6dd68c49f3016dcbfc58c7dba64690d97
dhirenaade <dhirenaade@gmail.com>
2020-06-09 16:28:57 +0530

    Issue #SB-18846 fix:updated package.json and replaced with correct Conditon.

--- player/public/coreplugins/org.sunbird.player.endpage-1.1/renderer/endpageApp.js
+++ player/public/coreplugins/org.sunbird.player.endpage-1.1/renderer/endpageApp.js
@@ -30,7 +30,7 @@ endPage.controller("endPageController", function($scope, $rootScope, $state,$ele
         $scope.licenseAttribute = $scope.playerMetadata.license || 'Licensed under CC By 4.0 license'
     };
     if(!$scope.isCordova){
-        if(_.isUndefined(globalConfig.context.userData)){
+        if(!_.isUndefined(globalConfig.context.userData)){
             $scope.currentPlayer = globalConfig.context.userData.firstName;
         }
     }
