ad6fc0b70960fb184f458882d580e6098598b739
sharath <sharaths.kashyap@gmail.com>
2020-12-28 18:33:05 +0530

    Issue #SB-21688 Fix:Migration Commit to Libraries

--- player/public/coreplugins/org.ekstep.videorenderer-1.1/renderer/plugin.js
+++ player/public/coreplugins/org.ekstep.videorenderer-1.1/renderer/plugin.js
@@ -49,7 +49,17 @@ org.ekstep.contentrenderer.baseLauncher.extend({
         var data = _.clone(content);
         this.heartBeatData.stageId = content.mimeType === 'video/x-youtube' ? 'youtubestage' : 'videostage';
         var globalConfigObj = EkstepRendererAPI.getGlobalConfig();
-        if (window.cordova || !isbrowserpreview) {
+        if(content.mimeType == 'audio/mp3') {
+            var regex = new RegExp("^(http|https)://", "i");
+            if (!regex.test(globalConfigObj.basepath)) {
+                var prefix_url = globalConfigObj.basepath || '';
+                path = prefix_url ? prefix_url + "/" + data.artifactUrl : data.artifactUrl;
+            } else {
+                path = data.artifactUrl;
+            }
+            skipValidation = true;
+            data.streamingUrl = false;
+        } else if (window.cordova || !isbrowserpreview) {
             var regex = new RegExp("^(http|https)://", "i");
             if (!regex.test(globalConfigObj.basepath)) {
                 var prefix_url = globalConfigObj.basepath || '';
