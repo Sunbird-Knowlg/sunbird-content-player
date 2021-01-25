bf29a5668c6c60166f00a8d46f1d0f867a648ed6
Gourav More <gourav_m@tekditechnologies.com>
2021-01-25 18:47:32 +0530

    Issue #SB-22653 fix: Mobile app: User is not navigating to next contents clicking on next and prev button on the content player if course has assessment contents

--- player/public/services/interfaceService.js
+++ player/public/services/interfaceService.js
@@ -267,13 +267,8 @@ org.ekstep.service.content = new (org.ekstep.service.mainService.extend({
 	checkMaxLimit: function (request) {
 		return new Promise(function (resolve, reject) {
 			org.ekstep.service.renderer.checkMaxLimit(request)
-				.then(function (contents) {
-					if (contents) {
-						resolve(contents)
-					} else {
-						// eslint-disable-next-line
-                        reject("Contents is not available.")
-					}
+				.then(function (data) {
+					resolve(data)
 				})
 				.catch(function (err) {
 					console.error(AppErrors.contetnPathFetch, err)
